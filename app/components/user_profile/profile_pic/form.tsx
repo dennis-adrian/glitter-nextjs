fed0d95ba8e2a7e47c27ecf7d46bb4789c961628
dennis-adrian <dennisguzmanbo@gmail.com>
2024-01-29 22:22:49 -0400

    Add progress bar

--- app/components/user_profile/profile_pic/form.tsx
+++ app/components/user_profile/profile_pic/form.tsx
@@ -1,14 +1,16 @@
 'use client';
 
+import { useState } from 'react';
+
 import {
   UserProfileType,
   updateProfileWithValidatedData,
 } from '@/app/api/users/actions';
-
 import { useEdgeStore } from '@/app/lib/edgestore';
+
 import { SingleImageDropzone } from '@/components/single-image-dropzone';
 import { Button } from '@/components/ui/button';
-import { useState } from 'react';
+import { Progress } from '@/components/ui/progress';
 
 export default function ProfilePictureForm({
   profile,
@@ -19,14 +21,16 @@ export default function ProfilePictureForm({
 }) {
   const [file, setFile] = useState<File>();
   const { edgestore } = useEdgeStore();
+  const [showProgress, setShowProgress] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   async function handleImageUpload() {
     if (file) {
       const res = await edgestore.publicFiles.upload({
         file,
         onProgressChange: (progress) => {
-          // you can use this to show a progress bar
-          console.log(progress);
+          setShowProgress(true);
+          setProgress(progress);
         },
       });
 
@@ -35,22 +39,31 @@ export default function ProfilePictureForm({
         imageUrl: res.url,
       });
 
-      if (result.success) onSuccess();
+      if (result.success) {
+        setShowProgress(false);
+        onSuccess();
+      }
     }
   }
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <div className="mt-4">
-        <SingleImageDropzone
-          width={200}
-          height={200}
-          value={file}
-          onChange={(file) => {
-            setFile(file);
-          }}
-        />
-      </div>
+      {showProgress ? (
+        <div className="flex items-center justify-center w-full h-[200px]">
+          <Progress value={progress} className="w-[60%]" />
+        </div>
+      ) : (
+        <div className="mt-4">
+          <SingleImageDropzone
+            width={200}
+            height={200}
+            value={file}
+            onChange={(file) => {
+              setFile(file);
+            }}
+          />
+        </div>
+      )}
       <Button className="w-full" onClick={handleImageUpload}>
         Guardar cambios
       </Button>
