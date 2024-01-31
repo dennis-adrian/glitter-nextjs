0098cef6ae0933658890106e262518ebdcb95c4e
dennis-adrian <dennisguzmanbo@gmail.com>
2024-01-31 00:14:09 -0400

    Add missing font

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
-import Image from 'next/image';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { londrinaSolid, roboto } from '@/ui/fonts';
-import LinkButton from '@/app/ui/link-button';
+import Image from "next/image";
+
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { londrinaSolid, roboto, junegull } from "@/ui/fonts";
+
+import LinkButton from "@/app/ui/link-button";
 
 export default function Home() {
   return (
     <main
-      className={`${roboto.className} flex min-h-screen flex-col items-center text-center pt-6 bg-gradient-to-b from-gradient-darker from-10% via-gradient-dark via-30% to-gradient-light to-50%`}
+      className={`${roboto.className} via-gradient-dark flex min-h-screen flex-col items-center bg-gradient-to-b from-blue-900 from-10% via-30% to-amber-50 to-50% pt-6 text-center`}
     >
       <section className="bg-hero-pattern p-2">
-        <h1 className="text-white font-heading text-5xl">¡Brillemos juntos!</h1>
+        <h1 className={`${junegull.className} text-5xl text-white`}>
+          ¡Brillemos juntos!
+        </h1>
         <p
-          className={`${londrinaSolid.className} text-white text-xl leading-6 max-w-xs m-auto py-4`}
+          className={`${londrinaSolid.className} m-auto max-w-xs py-4 text-xl leading-6 text-white`}
         >
           Crea experiencias que te inspiren, conecta con otros artistas y
-          celebra lo que eres{' '}
+          celebra lo que eres{" "}
         </p>
         <Image
           className="m-auto"
@@ -24,11 +28,11 @@ export default function Home() {
           height={300}
         />
       </section>
-      <section className="bg-secondary text-secondary-foreground p-8">
+      <section className="text-secondary-foreground bg-amber-50 p-8">
         <h1 className={`${londrinaSolid.className} text-4xl`}>
           Próximo Evento
         </h1>
-        <p className="text-xl leading-6 py-4">
+        <p className="py-4 text-xl leading-6">
           El 2 y 3 de marzo tendremos nuestra siguiente edición de Glitter
           Publicaremos la convocatoria en nuestras redes en enero
         </p>
@@ -40,11 +44,11 @@ export default function Home() {
           Síguenos en Instagram
         </LinkButton>
       </section>
-      <section className="bg-white w-screen py-8 px-2">
+      <section className="w-screen bg-white px-2 py-8">
         <h1 className={`${londrinaSolid.className} text-4xl`}>
           ¿Quiénes somos?
         </h1>
-        <p className="text-xl leading-6 py-4">
+        <p className="py-4 text-xl leading-6">
           Lorem ipsum dolor sit amet consectetur. Mauris dictumst quis bibendum
           a porttitor ut. Platea at ac nisi massa. Nec in lobortis nunc vel
           amet.
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
 import { Londrina_Solid, Roboto } from 'next/font/google';
+import localFont from 'next/font/local';
 
 export const londrinaSolid = Londrina_Solid({
   subsets: ['latin'],
@@ -8,4 +9,8 @@ export const londrinaSolid = Londrina_Solid({
 export const roboto = Roboto({
   subsets: ['latin'],
   weight: ['400'],
+});
+
+export const junegull = localFont({
+  src: '../fonts/junegull.otf',
 });
