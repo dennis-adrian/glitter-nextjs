c24cf1bd93ccddb6cad5d9f69bafd9d2b7bfa2fc
dennis-adrian <dennisguzmanbo@gmail.com>
2024-02-01 16:31:13 -0400

    Add changes to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import bg_image from "../public/img/bg_w_1280.png";
 export default function Home() {
   return (
     <div className="text-center">
-      <section className="relative flex flex-col items-center justify-center">
+      <section className="relative flex flex-col items-center justify-center p-8">
         <div className="-z-10">
           <Image
             alt="background image"
@@ -22,15 +22,15 @@ export default function Home() {
             }}
           />
         </div>
-        <div className="m-auto mt-8 flex max-w-md flex-col justify-center">
+        <div className="m-auto mt-2 flex max-w-md flex-col justify-center">
           <h1
-            className={`${junegull.className} inline-block bg-gradient-to-r from-pink-50 via-fuchsia-200 to-amber-200 bg-clip-text text-5xl text-transparent text-white`}
+            className={`${junegull.className} text-5xl text-white sm:text-6xl`}
           >
             ¡Brillemos juntos!
           </h1>
         </div>
         <p
-          className={`${londrinaSolid.className} m-auto max-w-xs py-4 text-xl leading-6 text-white`}
+          className={`${londrinaSolid.className} m-auto max-w-xs py-4 text-xl leading-6 text-white sm:text-2xl`}
         >
           Festival para que los artistas brillen
         </p>
@@ -42,25 +42,26 @@ export default function Home() {
           height={300}
         />
       </section>
-      <section className="text-secondary-foreground m-auto max-w-screen-md bg-amber-50 p-8">
+      <section className="text-secondary-foreground m-auto max-w-screen-sm rounded-lg bg-amber-50 p-8 sm:mt-16">
         <h1 className={`${londrinaSolid.className} text-4xl`}>
           Próximo Evento
         </h1>
         <p className="py-4 text-xl leading-6">
-          El 2 y 3 de marzo tendremos nuestra siguiente edición de Glitter.
+          El 2 y 3 de marzo tendremos nuestra siguiente edición de Glitter
         </p>
         <Button>
           <Link href="/sign_up">¡Quiero participar!</Link>
         </Button>
       </section>
-      <section className="m-auto max-w-screen-md bg-white px-2 py-8">
+      <section className="m-auto max-w-screen-md bg-white p-8 sm:mt-8">
         <h1 className={`${londrinaSolid.className} text-4xl`}>
           ¿Quiénes somos?
         </h1>
-        <p className="m-auto max-w-screen-md py-4 text-center text-xl leading-6">
-          Glitter es una productora de eventos artistísticos dedicada a
-          proporcionar un espacio seguro y acogedor para que ilustradores,
-          artistas y autores de comics puedan mostrar y vender su arte
+        <p className="m-auto max-w-screen-sm py-4 text-center text-xl leading-6">
+          <strong>Glitter</strong> es una productora de eventos artistísticos
+          dedicada a proporcionar un espacio seguro y acogedor para que
+          ilustradores, artistas y autores de cómics puedan mostrar y vender su
+          arte
         </p>
       </section>
     </div>
