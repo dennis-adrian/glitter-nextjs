24282c8627ecaf3cd033a836364698e06f54a4e4
dennis-adrian <dennisguzmanbo@gmail.com>
2024-02-25 15:14:24 -0400

    Add landing page carousel

---
+++ app/components/landing/carousel.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Autoplay from "embla-carousel-autoplay";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+
+const carouselItems = [
+  "/img/landing/carousel-item-1.png",
+  "/img/landing/carousel-item-2.png",
+  "/img/landing/carousel-item-3.png",
+  "/img/landing/carousel-item-4.png",
+  "/img/landing/carousel-item-5.png",
+  "/img/landing/carousel-item-6.png",
+  "/img/landing/carousel-item-7.png",
+  "/img/landing/carousel-item-8.png",
+  "/img/landing/carousel-item-9.png",
+  "/img/landing/carousel-item-10.png",
+];
+
+export default function LandingCarousel() {
+  return (
+    <Carousel
+      className="w-full"
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+      plugins={[
+        Autoplay({
+          delay: 5000,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {carouselItems.map((src, index) => (
+          <CarouselItem
+            className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
+            key={index}
+          >
+            <div className="flex justify-center">
+              <Image
+                className="rounded-lg"
+                alt={`carousel item ${index}`}
+                src={src}
+                height={450}
+                width={350}
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,15 @@ import { currentUser } from "@clerk/nextjs";
 import { fetchActiveFestival } from "@/app/api/festivals/actions";
 import LandingRedirectButton from "@/app/components/landing/redirect-button";
 import { FestivalInfo } from "@/app/components/landing/festival-info-card";
+import Carousel from "@/app/components/landing/carousel";
 
 export default async function Home() {
   const user = await currentUser();
   const festival = await fetchActiveFestival({ acceptedUsersOnly: true });
 
   return (
-    <div className="text-center">
-      <section className="max-w-[1400px] m-auto py-0 md:px-6">
+    <div className="m-auto max-w-[1400px] text-center text-lg md:text-2xl">
+      <section className="py-0 md:px-6">
         <div className="relative flex flex-col p-6 md:p-10 lg:p-20">
           <div className="-z-10">
             <Image
@@ -29,27 +30,27 @@ export default async function Home() {
               }}
             />
           </div>
-          <div className="flex flex-col md:flex-row text-glitter-blue-950 md:justify-between md:gap-16">
-            <div className="flex flex-col md:gap-8 md:text-left sm:p-6 md:backdrop-blur-sm">
+          <div className="text-glitter-blue-950 flex flex-col md:flex-row md:justify-between md:gap-16">
+            <div className="flex flex-col sm:p-6 md:gap-8 md:text-left md:backdrop-blur-sm">
               <div>
                 <div className="m-auto mt-2">
                   <span className={junegull.className}>
-                    <h1 className="text-5xl text-white sm:text-7xl text-shadow shadow-blue-950">
+                    <h1 className="text-shadow text-5xl text-white shadow-blue-950 sm:text-7xl">
                       ¡Brillemos juntos!
                     </h1>
                   </span>
                 </div>
-                <p className="m-auto py-4 md:py-0 text-xl leading-6 md:text-2xl font-semibold text-shadow-sm shadow-white">
+                <p className="text-shadow-sm m-auto py-4 text-xl font-semibold leading-6 shadow-white md:py-0 md:text-2xl">
                   Festival para que los artistas brillen
                 </p>
               </div>
-              <div className="hidden md:block bg-card/50 p-6 rounded-lg backdrop-blur-sm">
-                <div className="font-semibold text-3xl">Próximo Evento</div>
+              <div className="bg-card/50 hidden rounded-lg p-6 backdrop-blur-sm md:block">
+                <div className="text-3xl font-semibold">Próximo Evento</div>
                 <div className="text-xl">
                   <p>
                     No te quedes fuera y participa de{" "}
                     {festival ? (
-                      <span className="font-semibold whitespace-nowrap">
+                      <span className="whitespace-nowrap font-semibold">
                         {festival.name}
                       </span>
                     ) : (
@@ -76,9 +77,9 @@ export default async function Home() {
           </div>
         </div>
       </section>
-      <section className="md:hidden bg-primary-50 m-auto px-4 py-8">
-        <div className="font-semibold text-3xl">Próximo Evento</div>
-        <div className="text-lg mt-4">
+      <section className="bg-gradient-to-br from-primary-100 to-accent-50 px-3 py-8 md:py-14 md:hidden md:px-6">
+        <div className="text-4xl font-semibold">Próximo Evento</div>
+        <div className="mt-4 text-lg">
           <p>
             No te quedes fuera y participa de{" "}
             {festival ? (
@@ -93,16 +94,20 @@ export default async function Home() {
         {festival && <FestivalInfo festival={festival} />}
         <LandingRedirectButton className="md:hidden" userId={user?.id} />
       </section>
-      <section className="m-auto max-w-screen-md bg-white p-8 sm:mt-8">
-        <h1 className={`${londrinaSolid.className} text-4xl`}>
-          ¿Quiénes somos?
+      <section className="px-3 py-8 md:py-14 md:px-6">
+        <h1 className="text-4xl font-bold md:text-6xl">
+          Esto es el{" "}
+          <span className="from-primary to-accent whitespace-nowrap bg-gradient-to-br bg-clip-text tracking-tight text-transparent">
+            Festival Glitter
+          </span>
         </h1>
-        <p className="m-auto max-w-screen-sm py-4 text-center text-xl leading-6">
-          <strong>Glitter</strong> es una productora de eventos artísticos
-          dedicada a proporcionar un espacio seguro y acogedor para que
-          ilustradores, artistas y autores de cómics puedan mostrar y vender su
-          arte
+        <p className="my-2 leading-6">
+          un evento creado para brindar un espacio acogedor y seguro para
+          artistas
         </p>
+        <div className="py-4 md:py-8">
+          <Carousel />
+        </div>
       </section>
     </div>
   );
