341995c607306db360f2dbc099d23041e3d100bb
dennis-adrian <dennisguzmanbo@gmail.com>
2024-02-28 10:13:27 -0400

    Try to fix issue uploading file in node env

--- app/data/tickets/helpers.ts
+++ app/data/tickets/helpers.ts
@@ -1,17 +1,15 @@
+"use server";
+
 import { FestivalBase } from "@/app/api/festivals/definitions";
 import { VisitorWithTickets } from "@/app/data/visitors/actions";
 import { backendClient } from "@/app/lib/edgestore-server";
 
 export async function uploadQrCode(qrCodeUrl: string) {
-  console.log("is there anything wrong with the qrcodeulr?", qrCodeUrl);
   const blobPromise = await fetch(qrCodeUrl);
   const blob = await blobPromise.blob();
-  console.log("is the error inside the uploadQrCode function?", blob);
-  const file = new File([blob], "ticket-qrcode.png", { type: "image/png" });
-  console.log("is the error after the file is created?", file);
   const uploadedFile = await backendClient.publicFiles.upload({
     content: {
-      blob: file,
+      blob,
       extension: "png",
     },
   });
