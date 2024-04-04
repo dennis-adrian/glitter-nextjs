54991653070ad68dd42dfc0305224d88f2f5031c
dennis-adrian <dennisguzmanbo@gmail.com>
2024-04-04 18:54:41 -0400

    refactor: Simplify fetching user profile data
    Remove unnecessary conditional statements and simplify the logic for fetching user
    profile data by directly assigning the result of the fetchUserProfile function to
    the profile variable. This improves code readability and reduces redundancy.

--- app/user_profile/page.tsx
+++ app/user_profile/page.tsx
@@ -3,28 +3,15 @@ import { currentUser, SignedIn } from "@clerk/nextjs";
 import { fetchUserProfile } from "@/app/api/users/actions";
 import { ProfileType } from "@/app/api/users/definitions";
 
-import { Separator } from "@/app/components/ui/separator";
-import BirthdateField from "@/app/components/user_profile/birthdate/field";
-import EmailField from "@/app/components/user_profile/email/field";
-import NameField from "@/app/components/user_profile/name/field";
-import PhoneField from "@/app/components/user_profile/phone/field";
 import AnnouncementCard from "@/components/user_profile/announcements_cards/card";
 import PublicProfile from "@/components/user_profile/public_profile/profile";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/app/components/ui/card";
 import { PersonalInformation } from "@/app/components/user_profile/personal_information";
 
 async function UserProfile() {
   const user = await currentUser();
-  let profile: ProfileType | null = null;
+  let profile: ProfileType | null | undefined;
   if (user) {
-    const data = await fetchUserProfile(user.id);
-    profile = data.user!;
+    profile = await fetchUserProfile(user.id);
   }
 
   if (!profile) {
