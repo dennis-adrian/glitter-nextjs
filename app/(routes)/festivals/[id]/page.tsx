0cff64247b318ea6bc0930ec1b02fcf16ff368bd
dennis-adrian <dennisguzmanbo@gmail.com>
2024-04-10 19:34:22 -0400

    Improve event map

--- app/(routes)/festivals/[id]/page.tsx
+++ app/(routes)/festivals/[id]/page.tsx
@@ -148,18 +148,18 @@ export default async function Page({
               Distribución general del evento
             </h3>
             <Image
-              className="hidden md:block mx-auto"
-              alt="mascota del evento"
-              height={800}
-              src={generalMapMd}
-              width={595}
+              className="md:hidden mx-auto"
+              alt="mapa el evento"
+              height={545}
+              src={generalMapSm}
+              width={429}
             />
             <Image
-              className="md:hidden mx-auto"
-              alt="mascota del evento"
-              height={465}
-              src={generalMapSm}
-              width={360}
+              className="hidden md:block mx-auto"
+              alt="mapa del evento"
+              height={1091}
+              src={generalMapMd}
+              width={858}
             />
           </>
         )}
