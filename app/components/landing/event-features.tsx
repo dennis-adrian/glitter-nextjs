8c36677d6a72379756310173a547954ec1bd5fb5
dennis-adrian <dennisguzmanbo@gmail.com>
2024-04-10 21:24:02 -0400

    Add new elements to the landing page

--- app/components/landing/event-features.tsx
+++ app/components/landing/event-features.tsx
@@ -10,18 +10,10 @@ function Figure({
   caption: string;
 }) {
   return (
-    <div className="bg-gradient-to-b from-blue-200 to-white border border-blue-50 p-4 rounded-lg transition duration-300 ease-in-out hover:from-blue-300 hover:to-rose-100 hover:scale-105 hover:border-none">
-      <figure>
-        <Image
-          className="m-auto"
-          src={src}
-          alt={alt}
-          width={240}
-          height={240}
-        />
-        <figcaption className="font-semibold">{caption}</figcaption>
-      </figure>
-    </div>
+    <figure>
+      <Image className="m-auto" src={src} alt={alt} width={320} height={310} />
+      <figcaption className="font-semibold">{caption}</figcaption>
+    </figure>
   );
 }
 
@@ -29,22 +21,22 @@ export default function EventFeatures() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 pt-8 max-w-screen-lg m-auto">
       <Figure
-        src="/img/landing/samy-illustration.png"
+        src="/img/landing/mascot-illustration.png"
         alt="Samy ilustrador"
         caption="Ilustración"
       />
       <Figure
-        src="/img/landing/samy-pin.png"
+        src="/img/landing/mascot-pin.png"
         alt="Samy con un pin"
         caption="Pines"
       />
       <Figure
-        src="/img/landing/samy-comic.png"
+        src="/img/landing/mascot-comic.png"
         alt="Samy con un comic"
         caption="Cómics"
       />
       <Figure
-        src="/img/landing/samy-stickers.png"
+        src="/img/landing/mascot-stickers.png"
         alt="Samy con stickers"
         caption="Stickers"
       />
