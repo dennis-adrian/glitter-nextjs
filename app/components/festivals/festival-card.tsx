3731eece406787bcd3697bc453ac93c26ab07e0b
dennis-adrian <dennisguzmanbo@gmail.com>
2024-05-01 22:51:34 -0400

    Add new ui to festivals page

--- app/components/festivals/festival-card.tsx
+++ app/components/festivals/festival-card.tsx
@@ -1,37 +1,71 @@
-import ActiveFestivalForm from "@/app/components/festivals/activate-festival-form";
-import FestivalStatusBadge from "@/app/components/festivals/festival-status-badge";
+import {
+  ArchiveIcon,
+  BuildingIcon,
+  CalendarDaysIcon,
+  MapPinIcon,
+} from "lucide-react";
+
+import FestivalStatusBadge from "@/components/festivals/festival-status-badge";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/app/components/ui/card";
+} from "@/components/ui/card";
 import { FestivalBase } from "@/app/data/festivals/definitions";
 import { getFestivalDateLabel } from "@/app/helpers/next_event";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 export default function FestivalCard({ festival }: { festival: FestivalBase }) {
   return (
-    <Card className="shadow-md my-4">
+    <Card>
       <CardHeader>
-        <CardTitle className="flex gap-2">
-          {festival.name}
+        <div className="flex gap-2 items-start">
+          <CardTitle>{festival.name}</CardTitle>
           <FestivalStatusBadge status={festival.status} />
-        </CardTitle>
-        <CardDescription>{festival.description}</CardDescription>
+        </div>
+        <CardDescription>
+          {festival.description || "No definido"}
+        </CardDescription>
       </CardHeader>
-      <CardContent>
-        <p>
-          <strong>Fecha:</strong> {getFestivalDateLabel(festival)}
-        </p>
-        <p>
-          <strong>Lugar:</strong> {festival.locationLabel}
-        </p>
-        <p>
-          <strong>Ubicación:</strong> {festival.address}
-        </p>
-        {festival.status === "draft" && (
-          <ActiveFestivalForm festival={festival} />
+      <CardContent className="space-y-4">
+        {festival.status !== "archived" && (
+          <div className="p-4 border rounded-lg space-y-2">
+            <div className="flex items-center space-x-2">
+              <Label htmlFor="activate">Activar</Label>
+              <Switch id="activate" />
+            </div>
+            <div className="flex items-center space-x-2">
+              <Label htmlFor="pre-registration">Acreditación</Label>
+              <Switch id="pre-registration" />
+            </div>
+          </div>
+        )}
+        <div className="p-4 border rounded-lg space-y-3">
+          <h3 className="font-semibold text-xl">Detalles</h3>
+          <div>
+            <span className="flex gap-2 items-center">
+              <CalendarDaysIcon className="w-5 h-5" />{" "}
+              {getFestivalDateLabel(festival)}
+            </span>
+            <span className="flex gap-2 items-center">
+              <BuildingIcon className="w-5 h-5" />{" "}
+              {festival.locationLabel || "No definido"}
+            </span>
+            <span className="flex gap-2 items-center">
+              <MapPinIcon className="w-5 h-5" />{" "}
+              {festival.address || "No definido"}
+            </span>
+          </div>
+        </div>
+        {festival.status !== "archived" && (
+          <Button className="w-full" variant="outline">
+            <ArchiveIcon className="w-5 h-5 mr-2" />
+            Archivar
+          </Button>
         )}
       </CardContent>
     </Card>
--- app/dashboard/festivals/page.tsx
+++ app/dashboard/festivals/page.tsx
@@ -6,9 +6,11 @@ export default async function Page() {
   return (
     <div className="container p-4 md:p6">
       <h1 className="mb-2 text-2xl font-bold md:text-3xl">Festivales</h1>
-      {festivals.map((festival) => (
-        <FestivalCard key={festival.id} festival={festival} />
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {festivals.map((festival) => (
+          <FestivalCard key={festival.id} festival={festival} />
+        ))}
+      </div>
     </div>
   );
 }
