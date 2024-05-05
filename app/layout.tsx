cee48c786cf376b0b65736228f7bc43b44472f30
dennis-adrian <dennisguzmanbo@gmail.com>
2024-05-05 16:06:08 -0400

    Add uploadthing integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import Footer from "@/app/components/footer";
 
 import "./globals.css";
 import { getEnvLabel } from "./lib/config";
+import "@uploadthing/react/styles.css";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
