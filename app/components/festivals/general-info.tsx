68bcbdf2ace6442f8e47728652ffcadd22d037a4
dennis-adrian <dennisguzmanbo@gmail.com>
2024-06-19 14:33:34 -0400

    Add general map to general view

--- app/components/festivals/general-info.tsx
+++ app/components/festivals/general-info.tsx
@@ -39,43 +39,69 @@ export default function GeneralInfo(props: GeneralInfoProps) {
   const dates = props.festival.festivalDates;
 
   return (
-    <div className="flex gap-4 pt-4 md:p-6 sm:justify-center items-start flex-wrap sm:flex-row-reverse">
-      {props.festival.mascotUrl && (
-        <Image
-          className="mx-auto flex-grow-0"
-          alt="mascota del evento"
-          height={545}
-          src={props.festival.mascotUrl}
-          width={300}
-        />
-      )}
-      <div className="flex flex-wrap gap-4 py-4 flex-grow md:justify-around">
-        {dates &&
-          dates.length > 0 &&
-          dates.map((date) => <DateLabel key={date.id} date={date} />)}
-        <div className="flex gap-2">
-          <div className="w-12 h-12 rounded-sm border flex justify-center items-center">
-            <MapPinIcon className="w-7 h-7 text-muted-foreground" />
-          </div>
-          <div className="flex flex-col">
-            {props.festival.locationUrl ? (
-              <Link
-                className="flex gap-1 hover:underline"
-                href={props.festival.locationUrl}
-                target="_blank"
-              >
-                {props.festival.locationLabel}
-                <ArrowUpRightIcon className="h-4 w-4 text-muted-foreground" />
-              </Link>
-            ) : (
-              props.festival.locationLabel
-            )}
-            <span className="text-muted-foreground text-sm">
-              {props.festival.address}
-            </span>
+    <div>
+      <div className="flex gap-4 pt-4 md:p-6 sm:justify-center items-start flex-wrap sm:flex-row-reverse">
+        {props.festival.mascotUrl && (
+          <Image
+            className="mx-auto flex-grow-0"
+            alt="mascota del evento"
+            height={545}
+            src={props.festival.mascotUrl}
+            width={300}
+          />
+        )}
+        <div className="flex flex-wrap gap-4 py-4 flex-grow md:justify-around">
+          {dates &&
+            dates.length > 0 &&
+            dates.map((date) => <DateLabel key={date.id} date={date} />)}
+          <div className="flex gap-2">
+            <div className="w-12 h-12 rounded-sm border flex justify-center items-center">
+              <MapPinIcon className="w-7 h-7 text-muted-foreground" />
+            </div>
+            <div className="flex flex-col">
+              {props.festival.locationUrl ? (
+                <Link
+                  className="flex gap-1 hover:underline"
+                  href={props.festival.locationUrl}
+                  target="_blank"
+                >
+                  {props.festival.locationLabel}
+                  <ArrowUpRightIcon className="h-4 w-4 text-muted-foreground" />
+                </Link>
+              ) : (
+                props.festival.locationLabel
+              )}
+              <span className="text-muted-foreground text-sm">
+                {props.festival.address}
+              </span>
+            </div>
           </div>
         </div>
       </div>
+      {props.festival.generalMapUrl && (
+        <div>
+          <h2 className="font-semibold text-xl">Distribución General</h2>
+          <span className="flex flex-wrap text-muted-foreground text-sm">
+            <p className="mr-1">
+              Para ven en detalle los participantes y los sectores del evento
+              visita{" "}
+              <Link
+                className="text-primary-400 hover:underline"
+                href={`/festivals/${props.festival.id}?tab=sectors`}
+              >
+                Sectores y participantes
+              </Link>
+            </p>
+          </span>
+          <Image
+            className="mx-auto my-4"
+            alt="mapa del evento"
+            height={545}
+            src={props.festival.generalMapUrl}
+            width={300}
+          />
+        </div>
+      )}
     </div>
   );
 }
