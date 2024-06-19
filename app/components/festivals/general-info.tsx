3a54531b4b73c5894842655d056d26a82ffe467b
dennis-adrian <dennisguzmanbo@gmail.com>
2024-06-19 00:32:29 -0400

    Add location info

--- app/components/festivals/general-info.tsx
+++ app/components/festivals/general-info.tsx
@@ -4,7 +4,9 @@ import {
   FestivalWithDatesAndSectors,
 } from "@/app/data/festivals/definitions";
 import { formatDate } from "@/app/lib/formatters";
+import { ArrowUpRightIcon, MapPinIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 function DateLabel({ date }: { date: FestivalDate }) {
   const startDate = formatDate(date.startDate);
@@ -37,23 +39,43 @@ export default function GeneralInfo(props: GeneralInfoProps) {
   const dates = props.festival.festivalDates;
 
   return (
-    <div className="flex gap-4 sm:justify-center items-start flex-wrap sm:flex-row-reverse">
+    <div className="flex gap-4 pt-4 md:p-6 sm:justify-center items-start flex-wrap sm:flex-row-reverse">
       {props.festival.mascotUrl && (
         <Image
-          className="mx-auto"
+          className="mx-auto flex-grow-0"
           alt="mascota del evento"
           height={545}
           src={props.festival.mascotUrl}
           width={300}
         />
       )}
-      {dates && dates.length > 0 && (
-        <div className="flex flex-wrap gap-4 py-4">
-          {dates.map((date) => (
-            <DateLabel key={date.id} date={date} />
-          ))}
+      <div className="flex flex-wrap gap-4 py-4 flex-grow md:justify-around">
+        {dates &&
+          dates.length > 0 &&
+          dates.map((date) => <DateLabel key={date.id} date={date} />)}
+        <div className="flex gap-2">
+          <div className="w-12 h-12 rounded-sm border flex justify-center items-center">
+            <MapPinIcon className="w-7 h-7 text-muted-foreground" />
+          </div>
+          <div className="flex flex-col">
+            {props.festival.locationUrl ? (
+              <Link
+                className="flex gap-1 hover:underline"
+                href={props.festival.locationUrl}
+                target="_blank"
+              >
+                {props.festival.locationLabel}
+                <ArrowUpRightIcon className="h-4 w-4 text-muted-foreground" />
+              </Link>
+            ) : (
+              props.festival.locationLabel
+            )}
+            <span className="text-muted-foreground text-sm">
+              {props.festival.address}
+            </span>
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
