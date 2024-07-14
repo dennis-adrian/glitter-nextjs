6be98dede7456409eb8e49ac0a38c75d28e478b2
dennis-adrian <dennisguzmanbo@gmail.com>
2024-07-14 14:45:35 -0400

    Fix banner for twinkler

--- app/components/events/registration/form-banner.tsx
+++ app/components/events/registration/form-banner.tsx
@@ -1,44 +1,43 @@
-"use client";
-
-import Image from "next/image";
-
 import { FestivalWithDates } from "@/app/data/festivals/definitions";
 import { FestivalInfo } from "@/components/landing/festival-info-card";
+import Image from "next/image";
 
 export function FormBanner({ festival }: { festival: FestivalWithDates }) {
   const mascotSrc = festival.mascotUrl;
   return (
     <>
       <div className="text-primary-foreground relative mt-5 flex w-full items-center justify-center p-6 sm:mt-0">
-        <Image
-          className="-z-10 rounded-2xl object-cover"
-          src="/img/landing-banner-sm.png"
-          alt="Festival Image"
-          quality={100}
-          fill
-        />
+        {festival.festivalType === "twinkler" ? (
+          <Image
+            className="-z-10 rounded-2xl object-cover brightness-50"
+            src="/img/twinkler/twinkler-banner-500x500.png"
+            alt="Festival Image"
+            quality={100}
+            fill
+          />
+        ) : (
+          <Image
+            className="-z-10 rounded-2xl object-cover"
+            src="/img/landing-banner-sm.png"
+            alt="Festival Image"
+            quality={100}
+            fill
+          />
+        )}
         <div className="flex gap-2 sm:gap-4 lg:flex-col w-full items-center justify-between">
-          <div className="bg-white/30 h-fit w-full py-4 rounded-lg backdrop-blur-xl">
+          <div className="bg-black/5 h-fit w-full py-4 rounded-lg backdrop-blur-xl">
             <h1 className="text-center text-2xl font-semibold">
               {festival.name}
             </h1>
             <FestivalInfo className="py-2" festival={festival} />
           </div>
-          {mascotSrc ? (
+          {mascotSrc && (
             <Image
-              className="hidden md:block"
+              className="hidden md:block rounded-md"
               src={mascotSrc}
               alt="Mascota Glitter"
               height={320}
               width={320}
-            />
-          ) : (
-            <Image
-              className="hidden md:block"
-              src="/img/glitter-mascot.png"
-              alt="Mascota Glitter"
-              height={328}
-              width={255}
             />
           )}
         </div>
