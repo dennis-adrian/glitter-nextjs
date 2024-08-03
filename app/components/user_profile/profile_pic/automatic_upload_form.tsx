ba07afcd4ac53a664e1a8f1f4f9ef49047788f1c
dennis-adrian <dennisguzmanbo@gmail.com>
2024-08-02 20:34:15 -0400

    Add size variations to image uploader

--- app/components/user_profile/profile_pic/automatic_upload_form.tsx
+++ app/components/user_profile/profile_pic/automatic_upload_form.tsx
@@ -8,11 +8,14 @@ import { useEdgeStore } from "@/app/lib/edgestore";
 
 import { SingleImageDropzone } from "@/components/single-image-dropzone";
 import { Progress } from "@/components/ui/progress";
+import { cn } from "@/app/lib/utils";
 
 export default function AutomaticProfilePicUploadForm({
   profile,
+  size,
 }: {
   profile: ProfileType;
+  size?: "sm" | "md" | "lg";
 }) {
   const { edgestore } = useEdgeStore();
   const [showProgress, setShowProgress] = useState(false);
@@ -47,23 +50,37 @@ export default function AutomaticProfilePicUploadForm({
     }
   }
 
+  let imageSize = { width: 120, height: 120 };
+  let containerSize = "w-[120px] h-[120px]";
+  if (size === "md") {
+    imageSize = { width: 260, height: 260 };
+    containerSize = "w-[260px] h-[260px]";
+  }
+
+  if (size === "lg") {
+    imageSize = { width: 520, height: 520 };
+    containerSize = "w-[520px] h-[520px]";
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
+    <div className={cn("", containerSize)}>
       {showProgress ? (
-        <div className="flex items-center justify-center w-full h-[200px]">
-          <Progress value={progress} className="w-[60%]" />
+        <div className="flex flex-col gap-3 items-center justify-center w-full h-full border border-dashed">
+          <Progress value={progress} className="w-[90%]" />
+          <span className="text-muted-foreground text-sm">
+            Subiendo imagen...
+          </span>
         </div>
       ) : (
         <div className="mt-4 mb-6">
           <SingleImageDropzone
             canRemove={false}
-            width={120}
-            height={120}
             value={profile.imageUrl || ""}
             dropzoneOptions={{
               maxSize: 1024 * 1024 * 3, // 3MB,
             }}
             onChange={(file) => handleImageUpload(file)}
+            {...imageSize}
           />
         </div>
       )}
