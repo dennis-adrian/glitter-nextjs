e4ae9bd30c0f411156e03245b1cc4a12880bb336
dennis-adrian <dennisguzmanbo@gmail.com>
2024-08-10 12:20:02 -0400

    update styles

--- app/components/payments/payment-proof-upload.tsx
+++ app/components/payments/payment-proof-upload.tsx
@@ -2,17 +2,13 @@
 
 import { useState } from "react";
 
-import { useEdgeStore } from "@/app/lib/edgestore";
-
-import { SingleImageDropzone } from "@/components/single-image-dropzone";
-import { Button } from "@/components/ui/button";
-import { Progress } from "@/components/ui/progress";
 import { InvoiceWithPaymentsAndStand } from "@/app/data/invoices/defiinitions";
 import { CreatePaymentResponseType } from "@/app/api/payments/route";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
-import { UploadButton, UploadDropzone } from "@/app/vendors/uploadthing";
+import { UploadButton } from "@/app/vendors/uploadthing";
 import Image from "next/image";
+import { Loader2Icon } from "lucide-react";
 
 const handlePaymentCreation = async (
   invoice: InvoiceWithPaymentsAndStand,
@@ -29,7 +25,8 @@ const handlePaymentCreation = async (
     }),
   });
 
-  return (await res.json()) as CreatePaymentResponseType;
+  const data = await res.json();
+  return data as CreatePaymentResponseType;
 };
 export default function PaymentProofUpload({
   invoice,
@@ -37,7 +34,7 @@ export default function PaymentProofUpload({
   invoice: InvoiceWithPaymentsAndStand;
 }) {
   const router = useRouter();
-  const [voucherImageUrl, setVoucherImageUrl] = useState<string>(
+  const [voucherImageUrl, setVoucherImageUrl] = useState<string | null>(
     invoice.payments[0]?.voucherUrl,
   );
 
@@ -45,13 +42,14 @@ export default function PaymentProofUpload({
     <div className="flex flex-col gap-4">
       {voucherImageUrl ? (
         <Image
+          className="mx-auto"
           src={voucherImageUrl}
           alt="comprobante de pago"
           width={300}
           height={400}
         />
       ) : (
-        <div className="h-[300px] w-[300px] border border-dashed mx-auto flex justify-center items-center">
+        <div className="h-[200px] w-[200px] border border-dashed mx-auto flex justify-center items-center">
           <p className="text-xs text-muted-foreground text-center">
             Haz clic en el botón para subir el comprobante
           </p>
@@ -78,6 +76,34 @@ export default function PaymentProofUpload({
           } else {
             toast.error(response.message);
           }
+        }}
+        content={{
+          button({ ready, isUploading, uploadProgress }) {
+            if (isUploading && uploadProgress === 100) {
+              return (
+                <Loader2Icon className="w-4 h-4 text-white animate-spin" />
+              );
+            }
+            if (isUploading) return <div>{uploadProgress}%</div>;
+            if (ready) return <div>Elige una imagen</div>;
+            return "Cargando...";
+          },
+          allowedContent({ ready, isUploading }) {
+            if (!ready) return null;
+            if (isUploading) return "Subiendo imagen...";
+            return "Imagen hasta 4MB";
+          },
+        }}
+        appearance={{
+          button: ({ ready, isUploading }) => {
+            if (!ready) {
+              return "bg-primary text-xs";
+            }
+            if (isUploading) {
+              return "bg-primary text-xs after:bg-primary-400/60 after:text-white";
+            }
+            return "bg-primary text-xs hover:bg-primary-400";
+          },
         }}
       />
     </div>
