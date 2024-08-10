86a12fc97b98e36f012e942b0a568f373d569d18
dennis-adrian <dennisguzmanbo@gmail.com>
2024-08-10 09:12:39 -0400

    Refactor some code

--- app/emails/verification_confimation/email-template.tsx
+++ app/emails/verification_confimation/email-template.tsx
@@ -14,6 +14,7 @@ import { UserCategory } from "@/app/api/users/definitions";
 import FullFestivalBody from "./full-festival-body";
 import EmailFooter from "../email-footer";
 import { FestivalWithDates } from "@/app/data/festivals/definitions";
+import EmailHeader from "@/app/emails/email-header";
 
 interface FestivalActivationTemplateProps {
   name: string;
@@ -34,22 +35,21 @@ export default function VerificationConfirmationEmailTemplate(
       <Preview>Ya eres parte de la comunidad Glitter</Preview>
       <Body style={styles.main}>
         <Container style={styles.container}>
-          <Section style={styles.section}>
+          <EmailHeader />
+          <Section style={styles.sectionWithBanner}>
             <Text style={styles.text}>¡Hola {props.name}!</Text>
             <Text style={styles.text}>
               ¡Felicidades! Tu cuenta ha sido verificada
             </Text>
-            {!props.festival && (
+            {!props.festival ? (
               <RegularBody category={props.category} baseUrl={baseUrl} />
-            )}
-            {props.festival && !props.isFestivalFull && (
+            ) : !props.isFestivalFull ? (
               <ActiveFestivalBody
                 baseUrl={baseUrl}
                 festival={props.festival}
                 profileId={props.profileId}
               />
-            )}
-            {props.festival && props.isFestivalFull && (
+            ) : (
               <FullFestivalBody
                 baseUrl={baseUrl}
                 festival={props.festival}
@@ -68,4 +68,22 @@ VerificationConfirmationEmailTemplate.PreviewProps = {
   name: "John Doe",
   profileId: 90,
   category: "illustration",
+  festival: {
+    id: 12,
+    name: "Glitter 10ma edición",
+    reservationsStartDate: new Date("2024-08-10 22:00:00"),
+    festivalDates: [
+      {
+        id: 1,
+        startDate: new Date(),
+        endDate: new Date(),
+      },
+      {
+        id: 2,
+        startDate: new Date(),
+        endDate: new Date(),
+      },
+    ],
+  },
+  isFestivalFull: true,
 } as FestivalActivationTemplateProps;
