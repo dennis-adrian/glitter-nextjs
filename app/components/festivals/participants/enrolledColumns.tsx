87a8a9de99308d800faa296618bbef713d9db6af
dennis-adrian <dennisguzmanbo@gmail.com>
2024-10-14 10:55:24 -0300

    Fix table and add extra column to table

--- app/components/festivals/participants/enrolledColumns.tsx
+++ app/components/festivals/participants/enrolledColumns.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { BaseProfile } from "@/app/api/users/definitions";
+import CategoryBadge from "@/app/components/category-badge";
 import ProfileCell from "@/app/components/common/table/profile-cell";
 import { DataTableColumnHeader } from "@/app/components/ui/data_table/column-header";
 import { ColumnDef } from "@tanstack/react-table";
 
 export const columnTitles = {
   profile: "Perfil",
+  profileCategory: "Categoría",
 };
 
 export const columns: ColumnDef<BaseProfile>[] = [
@@ -18,5 +20,20 @@ export const columns: ColumnDef<BaseProfile>[] = [
       <DataTableColumnHeader column={column} title={columnTitles.profile} />
     ),
     cell: ({ row }) => <ProfileCell profile={row.original} />,
+  },
+  {
+    id: "profileCategory",
+    accessorFn: (user) => user.category,
+    header: ({ column }) => (
+      <DataTableColumnHeader
+        column={column}
+        title={columnTitles.profileCategory}
+      />
+    ),
+    cell: ({ row }) => (
+      <span>
+        <CategoryBadge category={row.original.category} />
+      </span>
+    ),
   },
 ];
