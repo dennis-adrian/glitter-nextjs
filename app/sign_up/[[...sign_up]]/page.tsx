42f80979abb8b58af46d75b8001ea4e546b2f42f
dennis-adrian <dennisguzmanbo@gmail.com>
2025-02-02 21:13:51 -0400

    Improve styles in sing in/up pages

--- app/sign_up/[[...sign_up]]/page.tsx
+++ app/sign_up/[[...sign_up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center px:3 md:py-6 py-4 h-full">
+    <div className="flex justify-center my-4 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
       <SignUp />
     </div>
   );
