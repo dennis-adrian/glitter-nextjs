2ebe1be1d7314b2c4f5feb99268937059deed18b
dennis-adrian <dennisguzmanbo@gmail.com>
2025-02-03 09:54:28 -0400

    Improve the behavior of the account creation confirmation modal

--- app/components/ui/navigation-sidebar.tsx
+++ app/components/ui/navigation-sidebar.tsx
@@ -61,9 +61,14 @@ const MobileSidebarItem = ({
 type NavigationSidebarProps = {
   profile?: ProfileType | null;
   children: React.ReactNode;
+  onCreateAccountClick: () => void;
 };
 
-const NavigationSidebar = ({ children, profile }: NavigationSidebarProps) => {
+const NavigationSidebar = ({
+  children,
+  profile,
+  onCreateAccountClick,
+}: NavigationSidebarProps) => {
   const { signOut } = useClerk();
   const pathname = usePathname();
 
@@ -182,14 +187,10 @@ const NavigationSidebar = ({ children, profile }: NavigationSidebarProps) => {
           <SignedOut>
             <div className="flex flex-col gap-1">
               <SheetClose asChild>
-                <RedirectButton
-                  className="w-full"
-                  href="/sign_up"
-                  variant="outline"
-                >
-                  <UserPlusIcon className="mr-2 h-6 w-6" />
+                <Button variant="outline" onClick={onCreateAccountClick}>
+                  <UserPlusIcon className="mr-2 h-5 w-5" />
                   Crear cuenta
-                </RedirectButton>
+                </Button>
               </SheetClose>
               <SheetClose asChild>
                 <RedirectButton className="w-full" href="/sign_in">
