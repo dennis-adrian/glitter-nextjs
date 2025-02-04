b01a1ee4c8a1cfba69b4d64e30071d53e26fddab
dennis-adrian <dennisguzmanbo@gmail.com>
2025-02-03 22:37:39 -0400

    Add loading pages for sign in/up pages

--- app/sign_up/[[...sign_up]]/page.tsx
+++ app/sign_up/[[...sign_up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center my-4 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
+    <div className="flex justify-center my-4">
       <SignUp />
     </div>
   );
