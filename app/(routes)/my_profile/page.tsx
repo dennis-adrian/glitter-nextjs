be364a8be40dbc6bae56f3b1a06e6019acd311ab
dennis-adrian <dennisguzmanbo@gmail.com>
2025-02-04 19:52:51 -0400

    Improve profile creation process

--- app/(routes)/my_profile/page.tsx
+++ app/(routes)/my_profile/page.tsx
@@ -1,11 +1,5 @@
-import { Suspense } from "react";
-import Loader from "@/app/components/loader";
 import MyProfilePage from "@/app/components/pages/my_profile/user-profile";
 
 export default async function UserProfile() {
-  return (
-    <Suspense fallback={<Loader />}>
-      <MyProfilePage />
-    </Suspense>
-  );
+  return <MyProfilePage />;
 }
--- app/(routes)/profile_verification/page.tsx
+++ app/(routes)/profile_verification/page.tsx
@@ -1,3 +1,5 @@
+import FetchExistingProfile from "@/app/(routes)/profile_verification/fetch-existing-profile";
+import Loading from "@/app/(routes)/profile_verification/loading";
 import { RedirectDrawer } from "@/app/components/redirect-drawer";
 import {
   createUserProfile,
@@ -5,35 +7,54 @@ import {
 } from "@/app/lib/users/actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import { z } from "zod";
 
-export default async function Page() {
+const searchParamsSchema = z.object({
+  create: z.coerce.boolean().optional(),
+});
+
+type SearchParams = z.infer<typeof searchParamsSchema>;
+
+export default async function Page(props: {
+  searchParams: Promise<SearchParams>;
+}) {
   const user = await currentUser();
   if (!user) {
     redirect("/sign_in");
   }
 
-  const profile = await fetchUserProfileByClerkId(user.id);
+  const searchParams = await props.searchParams;
+  const validatedSearchParams = searchParamsSchema.safeParse(searchParams);
+  if (!validatedSearchParams.success) {
+    return <h1>error</h1>;
+  }
 
-  if (profile) {
+  if (validatedSearchParams.data.create) {
+    const { success } = await createUserProfile({
+      clerkId: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.emailAddresses[0].emailAddress,
+      imageUrl: user.imageUrl,
+    });
+
+    if (!success) {
+      return (
+        <RedirectDrawer
+          title="¡Ups! Tuvimos un error"
+          message="No pudimos crear tu perfil. Te redirigiremos para que vuelvas a intentarlo."
+        />
+      );
+    }
+
     redirect("/my_profile");
   }
 
-  const { success } = await createUserProfile({
-    clerkId: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.emailAddresses[0].emailAddress,
-    imageUrl: user.imageUrl,
-  });
-
-  if (!success) {
-    return (
-      <RedirectDrawer
-        title="¡Ups! Tuvimos un error"
-        message="No pudimos encontrar o crear tu perfil. Te redirigiremos para que vuelvas a intentarlo."
-      />
-    );
-  }
-
-  redirect("/my_profile");
+  const profile = fetchUserProfileByClerkId(user.id);
+  return (
+    <Suspense fallback={<Loading />}>
+      <FetchExistingProfile profilePromise={profile} />
+    </Suspense>
+  );
 }
