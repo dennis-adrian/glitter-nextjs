f57b068cb7e4708da59978ba405e4394b45b7bbf
dennis-adrian <dennisguzmanbo@gmail.com>
2025-02-04 19:52:51 -0400

    Improve profile creation process

--- app/(routes)/profile_verification/loading.tsx
+++ app/(routes)/profile_verification/loading.tsx
@@ -1,11 +1,11 @@
 import { Loader2 } from "lucide-react";
 
-export default async function Loading() {
+export default function Loading() {
   return (
     <div className="min-h-[calc(100vh-64px-180px)] md:min-h-[calc(100vh-80px-148px)] flex justify-center items-center">
-      <div className="flex w-full justify-center items-center gap-2 flex-col my-8">
-        <Loader2 className="w-6 h-6 animate-spin" />
-        <p className="">Validando tu perfil</p>
+      <div className="flex w-full justify-center items-center gap-2 my-8 text-muted-foreground">
+        <Loader2 className="w-4 h-4 animate-spin" />
+        <p>Validando tu perfil</p>
       </div>
     </div>
   );
