bf2daa4279e3ceeac446a5c520401e555fb78b49
dennis-adrian <dennisguzmanbo@gmail.com>
2025-02-05 00:39:51 -0400

    Add modal to guide user to complete their profile

--- app/(routes)/my_profile/page.tsx
+++ app/(routes)/my_profile/page.tsx
@@ -1,13 +1,31 @@
 import { Skeleton } from "@/app/components/ui/skeleton";
+import CompleteProfileModal from "@/app/components/user_profile/complete-profile-modal";
 import PrivateProfileOverview from "@/app/components/user_profile/private_profile/overview";
 import PublicProfile from "@/app/components/user_profile/public_profile/profile";
 import UserProfileBanner from "@/app/components/users/user-profile-banner";
+import { fetchSubcategories } from "@/app/lib/subcategories/actions";
 import { getCurrentUserProfile } from "@/app/lib/users/helpers";
 import { Suspense } from "react";
+import { z } from "zod";
 
-export default async function UserProfile() {
+const searchParamsSchema = z.object({
+  completeProfile: z
+    .string()
+    .toLowerCase()
+    .transform((val) => val === "true"),
+});
+
+type SearchParams = z.infer<typeof searchParamsSchema>;
+
+export default async function Page(props: {
+  searchParams: Promise<SearchParams>;
+}) {
   const profile = await getCurrentUserProfile();
   if (!profile) return null;
+
+  const searchParams = await props.searchParams;
+  const validatedSearchParams = searchParamsSchema.safeParse(searchParams);
+  const subcategories = fetchSubcategories();
 
   return (
     <div className="container p-3 md:p-6 flex flex-col gap-2">
@@ -16,6 +34,11 @@ export default async function UserProfile() {
       </Suspense>
       <PublicProfile profile={profile} />
       <PrivateProfileOverview profile={profile} />
+      <CompleteProfileModal
+        subcategoriesPromise={subcategories}
+        profile={profile}
+        open={validatedSearchParams.data?.completeProfile}
+      />
     </div>
   );
 }
---
+++ app/components/user_profile/complete-profile-modal.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { ProfileType } from "@/app/api/users/definitions";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/components/ui/dialog";
+import CategoriesStep from "@/app/components/user_profile/creation-process/categories-step";
+import {
+  getMissingProfileFields,
+  getMissingProfileFieldsKeys,
+} from "@/app/lib/utils";
+import { Subcategory } from "@/app/lib/subcategories/definitions";
+import { use } from "react";
+import Categories from "@/app/components/user_profile/creation-process/categories";
+
+type CompleteProfileModalProps = {
+  children?: React.ReactNode;
+  profile: ProfileType;
+  open?: boolean;
+  subcategoriesPromise: Promise<Subcategory[]>;
+};
+
+export default function CompleteProfileModal({
+  children,
+  open,
+  profile,
+  subcategoriesPromise,
+}: CompleteProfileModalProps) {
+  const missingFields = getMissingProfileFieldsKeys(profile);
+  const subcategories = use(subcategoriesPromise);
+
+  return (
+    <Dialog open={open}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="p-3 md:p-6">
+        <DialogHeader>
+          <DialogTitle>Completa tu perfil</DialogTitle>
+        </DialogHeader>
+        <div>
+          {(missingFields.includes("category") ||
+            missingFields.includes("profileSubcategories")) && (
+            <Categories subcategories={subcategories} />
+          )}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
