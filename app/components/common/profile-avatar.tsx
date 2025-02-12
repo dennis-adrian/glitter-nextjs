1c1e291965011ce68632f822ae68e08f6c480ff9
dennis-adrian <dennisguzmanbo@gmail.com>
2025-02-12 00:24:11 -0400

    Add glitter stamp

--- app/components/common/profile-avatar.tsx
+++ app/components/common/profile-avatar.tsx
@@ -3,10 +3,12 @@ import { Avatar, AvatarImage } from "@/app/components/ui/avatar";
 import { Badge } from "@/app/components/ui/badge";
 import { getUserName } from "@/app/lib/users/utils";
 import { isNewProfile } from "@/app/lib/utils";
+import Image from "next/image";
 
 type ProfileAvatarProps = {
   className?: string;
   profile: BaseProfile & { participations: Participation[] };
+  showGlitterStamp?: boolean;
 };
 export default function ProfileAvatar(props: ProfileAvatarProps) {
   const { profile } = props;
@@ -22,6 +24,16 @@ export default function ProfileAvatar(props: ProfileAvatarProps) {
           }
         />
       </Avatar>
+      {props.showGlitterStamp && (
+        <div className="absolute -top-1 -right-2 bg-white rounded-full p-0.5">
+          <Image
+            src="/img/glitter-stamp.png"
+            alt="Glitter Stamp"
+            width={24}
+            height={24}
+          />
+        </div>
+      )}
       {isNewProfile(profile) && (
         <div className="absolute -bottom-2">
           <Badge className="bg-white text-foreground" variant="outline">
