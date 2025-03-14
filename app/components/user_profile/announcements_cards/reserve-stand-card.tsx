5cfd745f4c3d1527f04e340c945ef495455020a3
dennis-adrian <dennisguzmanbo@gmail.com>
2025-03-13 23:16:37 -0300

    Add terms and conditions quick access button

--- app/components/user_profile/announcements_cards/reserve-stand-card.tsx
+++ app/components/user_profile/announcements_cards/reserve-stand-card.tsx
@@ -6,7 +6,7 @@ import { formatDate } from "@/app/lib/formatters";
 import { RedirectButton } from "@/components/redirect-button";
 
 import BaseCard from "@/components/user_profile/announcements_cards/base-card";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon, NewspaperIcon } from "lucide-react";
 import { DateTime } from "luxon";
 
 type ReserveStandCardProps = {
@@ -37,18 +37,30 @@ export function ReserveStandCard(props: ReserveStandCardProps) {
     <BaseCard
       content={
         <p>
-          Ya puedes hacer tu reserva para la siguiente versión de Glitter.
-          Reserva tu espacio pulsando el botón
+          Ya puedes hacer tu reserva para nuestro próximo festival. También
+          puedes volver a leer los términos y condiciones.
         </p>
       }
       footer={
-        <RedirectButton
-          size="sm"
-          href={`/profiles/${props.profile.id}/festivals/${props.festival.id}/reservations/new`}
-        >
-          Ir a reservar espacio
-          <ArrowRightIcon className="ml-2 w-4 h-4" />
-        </RedirectButton>
+        <div className="w-full">
+          <RedirectButton
+            size="sm"
+            variant="outline"
+            className="w-full"
+            href={`/profiles/${props.profile.id}/festivals/${props.festival.id}/terms`}
+          >
+            Términos y condiciones
+            <NewspaperIcon className="ml-2 w-4 h-4" />
+          </RedirectButton>
+          <RedirectButton
+            size="sm"
+            className="w-full mt-2"
+            href={`/profiles/${props.profile.id}/festivals/${props.festival.id}/reservations/new`}
+          >
+            Reservar espacio
+            <ArrowRightIcon className="ml-2 w-4 h-4" />
+          </RedirectButton>
+        </div>
       }
     />
   );
