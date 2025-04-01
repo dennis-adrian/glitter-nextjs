3359e38888795efbf844b591f97d23a7d06a486a
dennis-adrian <dennisguzmanbo@gmail.com>
2025-04-01 12:37:48 -0400

    Add a short version of the activity description

--- app/(routes)/festivals/[id]/participants_activity/page.tsx
+++ app/(routes)/festivals/[id]/participants_activity/page.tsx
@@ -1,3 +1,41 @@
 export default function ParticipantsActivityPage() {
-  return <div className="container">ParticipantsActivityPage</div>;
+  return (
+    <div className="container">
+      <h1 className="text-2xl font-bold my-3">Sticker Print</h1>
+      <h2 className="text-lg font-bold my-2">¿Qué es el Sticker Print?</h2>
+      <p>
+        El Sticker Print es una actividad en la cual incentivaremos a los
+        asistentes del evento a coleccionar stickers. Como
+        artistas/ilustradores, sabemos que el potencial de los stickers es
+        enorme y como Productora Glitter, queremos que nuestro publico también
+        lo vea.
+      </p>
+      <h2 className="text-lg font-bold my-2">¿Cómo funciona?</h2>
+      <p>
+        Tendremos 4 diseños distintos de prints tamaño doble oficio que los
+        asistentes del evento podrán adquirir en el stand de Glitter al ingresar
+        al festival por un valor de 15Bs.
+      </p>
+      <p>
+        Cada diseño de print tendrá espacio para pegar 10 stickers
+        coleccionables de 10 ilustradores distintos. En total, al ser 4 diseños,
+        podremos contar con la participación de 40 ilustradores distintos.
+      </p>
+      <p>
+        Los participantes deberán recorrer el festival para encontrar a los
+        ilustradores que forman parte de su diseño de print y adquirir su
+        sticker coleccionable para pegar en su print.
+      </p>
+      <p>(poner imagenes de los prints?)</p>
+      <h2 className="text-lg font-bold my-2">
+        ¿Qué beneficios tiene para los ilustradores?
+      </h2>
+      <p>
+        Los ilustradores podrán mostrar su trabajo a un público mucho más grande
+        que el que habitualmente visitan en sus shows. Además, podrán
+        autografiar los prints a los participantes y crear una pieza única y
+        personalizada para cada participante.
+      </p>
+    </div>
+  );
 }
