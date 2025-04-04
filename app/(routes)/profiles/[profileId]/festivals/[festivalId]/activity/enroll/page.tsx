472deb96b623e6adea0318e56c22fcc78ccde8aa
dennis-adrian <dennisguzmanbo@gmail.com>
2025-04-04 19:11:20 -0400

    Add component that will be shown if the user is already enrolled

--- app/(routes)/profiles/[profileId]/festivals/[festivalId]/activity/enroll/page.tsx
+++ app/(routes)/profiles/[profileId]/festivals/[festivalId]/activity/enroll/page.tsx
@@ -2,6 +2,7 @@ import ActivityDetails from "@/app/components/festivals/festival_activities/acti
 import { getActiveFestival } from "@/app/lib/festivals/helpers";
 import { getCurrentUserProfile } from "@/app/lib/users/helpers";
 import { DateTime } from "luxon";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export default async function Page() {
@@ -35,6 +36,65 @@ export default async function Page() {
       </div>
     );
   }
+
+  const enrolledDesign = activity.details.find((detail) =>
+    detail.participants.some((participant) => participant.userId === user.id),
+  );
+
+  if (enrolledDesign) {
+    return (
+      <div className="container p-4 md:p-6">
+        <h1 className="text-2xl md:text-3xl font-bold mb-1">Sticker-Print</h1>
+        <p className="">
+          Muchas gracias por inscribirte a la actividad del Sticker-Print. Toma
+          nota de los siguientes detalles:
+        </p>
+        <ul className="list-disc list-inside">
+          <li>
+            La posición en la que irá tu sticker en el sticker-print es la
+            número{" "}
+            <strong>
+              {enrolledDesign.participants.findIndex(
+                (participant) => participant.userId === user.id,
+              ) + 1}
+              .
+            </strong>
+          </li>
+          <li>La medida de tu sticker tiene que ser de 4cm x 4cm.</li>
+          <li>
+            La cantidad minima de stickers para cumplir con la actividad es de
+            40.
+          </li>
+          <li>El diseño que elegiste es el siguiente:</li>
+        </ul>
+        {enrolledDesign.imageUrl && (
+          <div className="flex flex-col gap-1 justify-center items-center">
+            <Image
+              className="mx-auto mt-2 md:mt-4"
+              src={enrolledDesign.imageUrl}
+              alt={`Sticker Print ${enrolledDesign.id}`}
+              width={300}
+              height={480}
+            />
+            <p className="text-muted-foreground text-sm">
+              {enrolledDesign.participants
+                .map((participant) => {
+                  const name = participant.user.displayName;
+                  const position =
+                    enrolledDesign.participants.findIndex(
+                      (p) => p.userId === participant.userId,
+                    ) + 1;
+
+                  return `${position}. ${name}`;
+                })
+                .join(", ")}
+            </p>
+          </div>
+        )}
+      </div>
+    );
+  }
+
   return (
     <div className="container p-4 md:p-6">
       <h1 className="text-xl md:text-2xl font-bold mb-1">
