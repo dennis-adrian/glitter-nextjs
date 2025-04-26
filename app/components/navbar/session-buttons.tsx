44788fa056fe4fda6edbd47ada6d065659068a14
dennis-adrian <dennisguzmanbo@gmail.com>
2025-04-25 22:33:23 -0400

    DEV-84: Remove all user socials that are empty

--- app/components/navbar/session-buttons.tsx
+++ app/components/navbar/session-buttons.tsx
@@ -14,85 +14,78 @@ import UserDropdown from "@/app/components/ui/user-dropdown";
 import { isNoNavigationPage } from "@/app/lib/utils";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { InfoIcon, LogInIcon, UserPlusIcon } from "lucide-react";
-import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function SessionButtons({
-  profile,
+	profile,
 }: {
-  profile?: ProfileType | null;
+	profile?: ProfileType | null;
 }) {
-  const [open, setOpen] = useState(false);
-  const router = useRouter();
-  const pathname = usePathname();
+	const [open, setOpen] = useState(false);
+	const router = useRouter();
+	const pathname = usePathname();
 
-  if (isNoNavigationPage(pathname)) {
-    return null;
-  }
+	if (isNoNavigationPage(pathname)) {
+		return null;
+	}
 
-  return (
-    <div>
-      <SignedIn>
-        <UserDropdown profile={profile} />
-      </SignedIn>
-      <SignedOut>
-        <div className="flex gap-1">
-          <Button
-            variant="ghost"
-            size="sm"
-            className="px-1 md:px-2"
-            onClick={() => setOpen(true)}
-          >
-            <UserPlusIcon className="mr-2 h-5 w-5 hidden xl:block" />
-            Crear cuenta
-          </Button>
-          <RedirectButton href="/sign_in" size="sm" variant="outline">
-            <LogInIcon className="mr-1 h-5 w-5 hidden xl:block" />
-            Iniciar sesión
-          </RedirectButton>
-        </div>
-      </SignedOut>
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className="p-4 md:p-6">
-          <DialogHeader>
-            <DialogTitle className="text-center">
-              ¿Quieres crear una cuenta?
-            </DialogTitle>
-          </DialogHeader>
-          <div className="flex flex-col gap-2 my-2 text-center">
-            <InfoIcon className="w-12 h-12 mx-auto text-amber-500" />
-            <p className="leading-5">
-              Al crear una cuenta y completar tu perfil serás considerado para
-              participar como expositor en nuestros eventos
-            </p>
-            <p>
-              Si ya tienes una cuenta, haz clic{" "}
-              <Link className="underline text-blue-500" href="/sign_in">
-                aquí
-              </Link>
-            </p>
-          </div>
-          <DialogFooter className="flex flex-col-reverse md:flex-row w-full gap-2">
-            <Button
-              className="w-full"
-              variant="outline"
-              onClick={() => setOpen(false)}
-            >
-              Cancelar
-            </Button>
-            <Button
-              className="w-full"
-              onClick={() => {
-                setOpen(false);
-                router.push("/sign_up");
-              }}
-            >
-              Continuar
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
+	return (
+		<div>
+			<SignedIn>
+				<UserDropdown profile={profile} />
+			</SignedIn>
+			<SignedOut>
+				<div className="flex gap-1">
+					<Button
+						variant="ghost"
+						size="sm"
+						className="px-1 md:px-2"
+						onClick={() => setOpen(true)}
+					>
+						<UserPlusIcon className="mr-2 h-5 w-5 hidden xl:block" />
+						Crear cuenta
+					</Button>
+					<RedirectButton href="/sign_in" size="sm" variant="outline">
+						<LogInIcon className="mr-1 h-5 w-5 hidden xl:block" />
+						Iniciar sesión
+					</RedirectButton>
+				</div>
+			</SignedOut>
+			<Dialog open={open} onOpenChange={setOpen}>
+				<DialogContent className="p-4 md:p-6">
+					<DialogHeader>
+						<DialogTitle className="text-center">
+							¿Quieres crear una cuenta?
+						</DialogTitle>
+					</DialogHeader>
+					<div className="flex flex-col gap-2 my-2 text-center">
+						<InfoIcon className="w-12 h-12 mx-auto text-amber-500" />
+						<p className="leading-5">
+							Al crear una cuenta y completar tu perfil serás considerado para
+							participar como expositor en nuestros eventos
+						</p>
+					</div>
+					<DialogFooter className="flex flex-col-reverse md:flex-row w-full gap-2">
+						<Button
+							className="w-full"
+							variant="outline"
+							onClick={() => setOpen(false)}
+						>
+							Cancelar
+						</Button>
+						<Button
+							className="w-full"
+							onClick={() => {
+								setOpen(false);
+								router.push("/sign_up");
+							}}
+						>
+							Continuar
+						</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</div>
+	);
 }
