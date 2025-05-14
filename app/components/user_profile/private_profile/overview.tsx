0627de1b293c7668452dd054ca93ee97d11bdaa0
nelwey <nelwey1234@gmail.com>
2025-05-14 19:59:14 +0300

    [dev-98-bug] fix date on profile

--- app/components/user_profile/private_profile/overview.tsx
+++ app/components/user_profile/private_profile/overview.tsx
@@ -15,6 +15,7 @@ import { ShowField } from "@/app/components/user_profile/show-field";
 import { formatDate } from "@/app/lib/formatters";
 import { genderLabels } from "@/app/lib/utils";
 import SocialMediaBadge from "@/app/components/social-media-badge";
+import { DateTime } from "luxon";
 
 export default function PrivateProfileOverview({
   profile,
@@ -23,11 +24,10 @@ export default function PrivateProfileOverview({
 }) {
   let age = 0;
   if (profile.birthdate) {
-    const calculation = formatDate(new Date()).minus({
-      years: formatDate(profile.birthdate).year,
-    });
-
-    age = calculation.year;
+    const birthDate = DateTime.fromJSDate(new Date(profile.birthdate));
+    const now = DateTime.now();
+    age = now.diff(birthDate, 'years').years;
+    age = Math.floor(age); 
   }
 
   return (
