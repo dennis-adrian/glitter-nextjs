3b12705a3c5982009d6a672e70ac0cb9a6f4e681
dennis-adrian <dennisguzmanbo@gmail.com>
2025-05-15 23:19:19 -0400

    DEV-105: Add links to orders in navigation menus

--- app/components/organisms/mobile-sidebar.tsx
+++ app/components/organisms/mobile-sidebar.tsx
@@ -16,17 +16,18 @@ import {
 } from "@/app/components/ui/sheet";
 import { Button } from "@/app/components/ui/button";
 import {
-  AlbumIcon,
-  BookImageIcon,
-  BoxesIcon,
-  CalendarCheck2Icon,
-  CalendarIcon,
-  CreditCardIcon,
-  HomeIcon,
-  LogOutIcon,
-  StickerIcon,
-  TagsIcon,
-  UsersIcon,
+	AlbumIcon,
+	BookImageIcon,
+	BoxesIcon,
+	CalendarCheck2Icon,
+	CalendarIcon,
+	CreditCardIcon,
+	HomeIcon,
+	LogOutIcon,
+	ShirtIcon,
+	StickerIcon,
+	TagsIcon,
+	UsersIcon,
 } from "lucide-react";
 import { ProfileType } from "@/app/api/users/definitions";
 import GlitterLogo from "@/app/components/landing/glitter-logo";
@@ -63,100 +64,104 @@ const MobileSidebar = ({ children, profile }: MobileSidebarProps) => {
     return null;
 
   return (
-    <Sheet>
-      <SheetTrigger
-        className="cursor-default hover:bg-primary-100/30 hover:text-primary-500"
-        variant="outline"
-        size="icon"
-      >
-        {children}
-      </SheetTrigger>
-      <SheetContent side="left">
-        <SheetHeader>
-          <SheetTitle>
-            <SheetClose>
-              <Link href="/">
-                <GlitterLogo variant="dark" height={48} width={48} />
-              </Link>
-            </SheetClose>
-          </SheetTitle>
-        </SheetHeader>
-        <Separator className="my-2" />
-        <ul className="flex flex-col">
-          <MobileSidebarItem href="/">
-            <HomeIcon className="mr-2 h-6 w-6" />
-            Inicio
-          </MobileSidebarItem>
-          <MobileSidebarItem href="/next_event">
-            <CalendarCheck2Icon className="mr-2 h-6 w-6" />
-            Próximo Evento
-          </MobileSidebarItem>
-          <MobileSidebarItem href="/festivals">
-            <BookImageIcon className="mr-2 h-6 w-6" />
-            Festivales
-          </MobileSidebarItem>
-          <div className="ml-4">
-            <MobileSidebarItem href="/festivals/festicker">
-              <StickerIcon className="mr-2 h-6 w-6" />
-              Festicker
-            </MobileSidebarItem>
-          </div>
-          <MobileSidebarItem href="/festivals/categories">
-            <BoxesIcon className="w-6 h-6 mr-2" />
-            Categorías Glitter
-          </MobileSidebarItem>
-          {profile && profile.role === "admin" && (
-            <>
-              <MobileSidebarItem href="/dashboard">
-                <h4 className="text-lg">Dashboard</h4>
-              </MobileSidebarItem>
-              <div className="ml-4">
-                <MobileSidebarItem href="/dashboard/users?limit=10&offset=0&includeAdmins=false&status=pending&sort=updatedAt&direction=desc&profileCompletion=complete">
-                  <UsersIcon className="mr-2 h-6 w-6" />
-                  Usuarios
-                </MobileSidebarItem>
-                <MobileSidebarItem href="/dashboard/reservations">
-                  <AlbumIcon className="mr-2 h-6 w-6" />
-                  Reservas
-                </MobileSidebarItem>
-                <MobileSidebarItem href="/dashboard/payments">
-                  <CreditCardIcon className="mr-2 h-6 w-6" />
-                  Pagos
-                </MobileSidebarItem>
-                <MobileSidebarItem href="/dashboard/festivals">
-                  <CalendarIcon className="mr-2 h-6 w-6" />
-                  Festivales
-                </MobileSidebarItem>
-                <MobileSidebarItem href="/dashboard/subcategories">
-                  <BoxesIcon className="mr-2 h-6 w-6" />
-                  Subcategorías
-                </MobileSidebarItem>
-                <MobileSidebarItem href="/dashboard/tags">
-                  <TagsIcon className="mr-2 h-6 w-6" />
-                  Etiquetas
-                </MobileSidebarItem>
-              </div>
-            </>
-          )}
-          <SignedIn>
-            <Separator className="my-2" />
-            <SheetClose asChild>
-              <Button
-                className="p-2"
-                onClick={() => signOut(() => router.push("/"))}
-                variant="ghost"
-              >
-                <LogOutIcon className="mr-2 h-6 w-6" />
-                <span className="w-full text-left text-base font-normal">
-                  Cerrar Sesión
-                </span>
-              </Button>
-            </SheetClose>
-          </SignedIn>
-        </ul>
-      </SheetContent>
-    </Sheet>
-  );
+		<Sheet>
+			<SheetTrigger
+				className="cursor-default hover:bg-primary-100/30 hover:text-primary-500"
+				variant="outline"
+				size="icon"
+			>
+				{children}
+			</SheetTrigger>
+			<SheetContent side="left">
+				<SheetHeader>
+					<SheetTitle>
+						<SheetClose>
+							<Link href="/">
+								<GlitterLogo variant="dark" height={48} width={48} />
+							</Link>
+						</SheetClose>
+					</SheetTitle>
+				</SheetHeader>
+				<Separator className="my-2" />
+				<ul className="flex flex-col">
+					<MobileSidebarItem href="/">
+						<HomeIcon className="mr-2 h-6 w-6" />
+						Inicio
+					</MobileSidebarItem>
+					<MobileSidebarItem href="/next_event">
+						<CalendarCheck2Icon className="mr-2 h-6 w-6" />
+						Próximo Evento
+					</MobileSidebarItem>
+					<MobileSidebarItem href="/festivals">
+						<BookImageIcon className="mr-2 h-6 w-6" />
+						Festivales
+					</MobileSidebarItem>
+					<div className="ml-4">
+						<MobileSidebarItem href="/festivals/festicker">
+							<StickerIcon className="mr-2 h-6 w-6" />
+							Festicker
+						</MobileSidebarItem>
+					</div>
+					<MobileSidebarItem href="/festivals/categories">
+						<BoxesIcon className="w-6 h-6 mr-2" />
+						Categorías Glitter
+					</MobileSidebarItem>
+					{profile && profile.role === "admin" && (
+						<>
+							<MobileSidebarItem href="/dashboard">
+								<h4 className="text-lg">Dashboard</h4>
+							</MobileSidebarItem>
+							<div className="ml-4">
+								<MobileSidebarItem href="/dashboard/users?limit=10&offset=0&includeAdmins=false&status=pending&sort=updatedAt&direction=desc&profileCompletion=complete">
+									<UsersIcon className="mr-2 h-6 w-6" />
+									Usuarios
+								</MobileSidebarItem>
+								<MobileSidebarItem href="/dashboard/reservations">
+									<AlbumIcon className="mr-2 h-6 w-6" />
+									Reservas
+								</MobileSidebarItem>
+								<MobileSidebarItem href="/dashboard/payments">
+									<CreditCardIcon className="mr-2 h-6 w-6" />
+									Pagos
+								</MobileSidebarItem>
+								<MobileSidebarItem href="/dashboard/festivals">
+									<CalendarIcon className="mr-2 h-6 w-6" />
+									Festivales
+								</MobileSidebarItem>
+								<MobileSidebarItem href="/dashboard/orders">
+									<ShirtIcon className="mr-2 h-6 w-6" />
+									Pedidos
+								</MobileSidebarItem>
+								<MobileSidebarItem href="/dashboard/subcategories">
+									<BoxesIcon className="mr-2 h-6 w-6" />
+									Subcategorías
+								</MobileSidebarItem>
+								<MobileSidebarItem href="/dashboard/tags">
+									<TagsIcon className="mr-2 h-6 w-6" />
+									Etiquetas
+								</MobileSidebarItem>
+							</div>
+						</>
+					)}
+					<SignedIn>
+						<Separator className="my-2" />
+						<SheetClose asChild>
+							<Button
+								className="p-2"
+								onClick={() => signOut(() => router.push("/"))}
+								variant="ghost"
+							>
+								<LogOutIcon className="mr-2 h-6 w-6" />
+								<span className="w-full text-left text-base font-normal">
+									Cerrar Sesión
+								</span>
+							</Button>
+						</SheetClose>
+					</SignedIn>
+				</ul>
+			</SheetContent>
+		</Sheet>
+	);
 };
 
 export default MobileSidebar;
