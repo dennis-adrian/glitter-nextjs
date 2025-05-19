d03741c698683d310d259c97a65683c3aa8d826a
dennis-adrian <dennisguzmanbo@gmail.com>
2025-05-18 21:06:08 -0400

    DEV-104: Add form to item to be able to create order with different amounts of producs

--- app/(routes)/store/page.tsx
+++ app/(routes)/store/page.tsx
@@ -1,19 +1,19 @@
 import StoreProducts from "@/app/components/organisms/store-products";
 
 export default function StorePage() {
-	return (
-		<div className="container px-3 py-6">
-			<div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4">
-				<div>
-					<h1 className="text-4xl font-bold tracking-tight">
-						Tiendita Glitter
-					</h1>
-					<p className="text-muted-foreground mt-2">
-						Consigue mercha oficial de nuestros festivales
-					</p>
-				</div>
-			</div>
-			<StoreProducts />
-		</div>
-	);
+  return (
+    <div className="container px-3 py-6">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4">
+        <div>
+          <h1 className="text-2xl md:text-4xl font-bold tracking-tight">
+            Tiendita Glitter
+          </h1>
+          <p className="text-muted-foreground mt-2">
+            Consigue mercha oficial de nuestros festivales
+          </p>
+        </div>
+      </div>
+      <StoreProducts />
+    </div>
+  );
 }
