adf80dbb3a3ae55f9cceeff3724bd50669a78361
dennis-adrian <dennisguzmanbo@gmail.com>
2025-06-19 17:36:08 -0400

    DEV-117: Improve styling and add tooltip for participants names

--- app/components/festivals/public-festival-activity-detail.tsx
+++ app/components/festivals/public-festival-activity-detail.tsx
@@ -4,6 +4,7 @@ import { ReservationWithParticipantsAndUsersAndStand } from "@/app/api/reservati
 import { Avatar, AvatarImage } from "@/app/components/ui/avatar";
 import { ActivityDetailsWithParticipants } from "@/app/lib/festivals/definitions";
 import { useMemo } from "react";
+import { Tooltip } from "react-tooltip";
 
 type PublicFestivalActivityDetailProps = {
 	detail: ActivityDetailsWithParticipants;
@@ -41,21 +42,30 @@ export default function PublicFestivalActivityDetail({
 	);
 
 	return (
-		<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+		<div className="grid xxs:grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
 			{participantCardData.map((participant) => {
 				return (
 					<div
 						key={participant.participantId}
 						className="flex items-center gap-2 bg-card border border-border rounded-md p-2 shadow-sm"
 					>
-						<Avatar className="w-12 h-12">
+						<Avatar className="w-10 md:w-16 h-10 md:h-16">
 							<AvatarImage
 								src={participant.participantImageUrl || ""}
 								alt={participant.participantName || "avatar de usuario"}
 							/>
 						</Avatar>
-						<div>
-							<h3>{participant.participantName}</h3>
+						<div className="flex flex-col gap-1 max-w-fit">
+							<Tooltip
+								id={`participant-${participant.participantId}`}
+								content={participant.participantName}
+							/>
+							<h3
+								data-tooltip-id={`participant-${participant.participantId}`}
+								className="leading-tight text-sm md:text-base xxs:max-w-full xs:max-w-[95px] sm:max-w-full overflow-hidden text-ellipsis"
+							>
+								{participant.participantName}
+							</h3>
 							<p className="text-sm text-muted-foreground">
 								{participant.standLabel}
 							</p>
