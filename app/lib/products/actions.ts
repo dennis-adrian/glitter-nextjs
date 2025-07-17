c7b1968796993e349d88cc9f24e42d549eeeab6d
dennis-adrian <dennisguzmanbo@gmail.com>
2025-07-17 14:26:49 -0400

    Order products in store by created at date descending

--- app/lib/products/actions.ts
+++ app/lib/products/actions.ts
@@ -4,7 +4,9 @@ import { db } from "@/db";
 
 export async function fetchProducts() {
 	try {
-		return await db.query.products.findMany();
+		return await db.query.products.findMany({
+			orderBy: (products, { desc }) => [desc(products.createdAt)],
+		});
 	} catch (error) {
 		console.error(error);
 		return [];
