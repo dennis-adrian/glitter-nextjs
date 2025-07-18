6530e37a0ad8661adda4d4959243e1ea3c5f9523
dennis-adrian <dennisguzmanbo@gmail.com>
2025-07-17 21:05:49 -0400

    DEV-133: Downgrade nextjs to 15.3 to prevent font loading issues

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,21 +1,11 @@
-import { Inter, Londrina_Solid, Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
-export const londrinaSolid = Londrina_Solid({
-  subsets: ["latin"],
-  weight: ["300"],
-});
-
-export const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400"],
-});
-
 export const junegull = localFont({
-  src: "../fonts/junegull.otf",
+	src: "../fonts/junegull.otf",
 });
 
 export const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
+	subsets: ["latin"],
+	variable: "--font-inter",
 });
