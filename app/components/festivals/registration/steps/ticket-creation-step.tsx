1816b870ca39485ee0a773df2746ed4409f5b105
dennis-adrian <dennisguzmanbo@gmail.com>
2025-08-03 14:13:44 -0400

    Revert "bugfix; temporarily removing date validation"
    This reverts commit cb4cec9c0f9e7ed4b9fd8ad8b3fcad192f2b13b1.

--- app/components/festivals/registration/steps/ticket-creation-step.tsx
+++ app/components/festivals/registration/steps/ticket-creation-step.tsx
@@ -15,60 +15,60 @@ type TicketCreationStepProps = {
 };
 
 export default function TicketCreationStep(props: TicketCreationStepProps) {
-	if (!props.visitor?.id) return null;
+  if (!props.visitor?.id) return null;
 
-	// const ticketDate = props.festival.festivalDates.find((festivalDate) => {
-	//   return formatDate(festivalDate.startDate)
-	//     .startOf("day")
-	//     .equals(formatDate(new Date()).startOf("day"));
-	// });
+  const ticketDate = props.festival.festivalDates.find((festivalDate) => {
+    return formatDate(festivalDate.startDate)
+      .startOf("day")
+      .equals(formatDate(new Date()).startOf("day"));
+  });
 
-	// if (!ticketDate) {
-	//   return (
-	//     <div className="text-center text-sm md:text-lg border-2 border-dotted border-muted p-4 rounded-md text-muted-foreground">
-	//       No tenemos entradas disponibles para hoy
-	//     </div>
-	//   );
-	// }
+  if (!ticketDate) {
+    return (
+      <div className="text-center text-sm md:text-lg border-2 border-dotted border-muted p-4 rounded-md text-muted-foreground">
+        No tenemos entradas disponibles para hoy
+      </div>
+    );
+  }
 
-	const tickets = getVisitorFestivalTickets(props.visitor, props.festival);
-	const currentDayTicket = tickets.find((ticket) => {
-		return formatDate(ticket.date)
-			.startOf("day")
-			.equals(formatDate(new Date()).startOf("day"));
-	});
+  const tickets = getVisitorFestivalTickets(props.visitor, props.festival);
+  const currentDayTicket = tickets.find((ticket) => {
+    return formatDate(ticket.date)
+      .startOf("day")
+      .equals(formatDate(new Date()).startOf("day"));
+  });
 
-	return (
-		<div className="flex flex-col gap-2">
-			<h1 className="text-lg md:text-2xl font-bold text-center">
-				¡Gracias por visitarnos, {props.visitor.firstName}!
-			</h1>
-			{currentDayTicket ? (
-				<div>
-					<div className="text-sm md:text-base text-center mb-1">
-						Muestra tu entrada en puerta para ingresar al evento
-					</div>
-					<Tickets
-						visitor={props.visitor}
-						tickets={tickets}
-						festival={props.festival}
-					/>
-				</div>
-			) : (
-				<div className="flex flex-col gap-4">
-					<div className="text-center text-sm md:text-lg border-2 border-dotted border-muted p-4 rounded-md text-muted-foreground">
-						Aún no tienes entradas para este día
-					</div>
-					<EventDayTicketCreationForm
-						festival={props.festival}
-						numberOfVisitors={props.numberOfVisitors}
-						visitor={props.visitor}
-						onSuccess={(visitor) => {
-							props.onSuccess(visitor);
-						}}
-					/>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="text-lg md:text-2xl font-bold text-center">
+        ¡Gracias por visitarnos, {props.visitor.firstName}!
+      </h1>
+      {currentDayTicket ? (
+        <div>
+          <div className="text-sm md:text-base text-center mb-1">
+            Muestra tu entrada en puerta para ingresar al evento
+          </div>
+          <Tickets
+            visitor={props.visitor}
+            tickets={tickets}
+            festival={props.festival}
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          <div className="text-center text-sm md:text-lg border-2 border-dotted border-muted p-4 rounded-md text-muted-foreground">
+            Aún no tienes entradas para este día
+          </div>
+          <EventDayTicketCreationForm
+            festival={props.festival}
+            numberOfVisitors={props.numberOfVisitors}
+            visitor={props.visitor}
+            onSuccess={(visitor) => {
+              props.onSuccess(visitor);
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
