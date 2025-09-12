f6b0e58760538d37d2ad288c413e857937e63030
nelwey <nelwey1234@gmail.com>
2025-09-12 22:40:38 +0300

    [DEV-144] added vencimiento column

--- app/components/reservations/columns.tsx
+++ app/components/reservations/columns.tsx
@@ -13,7 +13,7 @@ import { ActionsCell } from "@/app/components/reservations/cells/actions";
 import { ReservationStatus } from "@/app/components/reservations/cells/status";
 import { Checkbox } from "@/app/components/ui/checkbox";
 import { DataTableColumnHeader } from "@/app/components/ui/data_table/column-header";
-import { formatFullDate } from "@/app/lib/formatters";
+import { formatDateWithTime, formatFullDate } from "@/app/lib/formatters";
 import ProfileQuickViewInfo from "@/app/components/users/profile-quick-view-info";
 import { Avatar, AvatarImage } from "@/app/components/ui/avatar";
 import PaymentStatus from "@/app/components/reservations/cells/payment-status";
@@ -26,6 +26,7 @@ export const columnTitles = {
   stand: "Espacio",
   status: "Estado de la Reserva",
   paymentStatus: "Estado del Pago",
+  expiration: "Vencimiento",
   collaborators: "Colaboradores",
 };
 
@@ -159,12 +160,23 @@ export const columns: ColumnDef<FullReservation>[] = [
     cell: ({ row }) => <PaymentStatus reservation={row.original} />,
   },
   {
+    accessorKey: "expiration",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title={columnTitles.expiration} />
+    ),
+    cell: ({ row }) => {
+      const createdAt = new Date(row.original.createdAt);
+      const expiration = new Date(createdAt.getTime() + 120 * 60 * 60 * 1000);
+      return formatDateWithTime(expiration);
+    },
+  },
+  {
     accessorKey: "createdAt",
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title={columnTitles.createdAt} />
     ),
     cell: ({ row }) => {
-      return formatFullDate(row.original.createdAt);
+      return formatDateWithTime(row.original.createdAt);
     },
   },
   {
