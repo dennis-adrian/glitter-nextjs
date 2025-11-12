fb92c319616686a028f4d8cb7a40a0737e2428ab
dennis-adrian <dennisguzmanbo@gmail.com>
2025-11-12 11:33:16 -0300

    DEV-154: Update preview of reservation reminder email

--- app/emails/reservation-reminder.tsx
+++ app/emails/reservation-reminder.tsx
@@ -26,61 +26,64 @@ export default function ReservationReminderTemplate(
   const userName = getUserName(task.profile);
 
   return (
-    <Html>
-      <Head />
-      <Preview>Completa tu perfil para participar de nuestros eventos</Preview>
-      <Body style={styles.main}>
-        <Container style={styles.container}>
-          <EmailHeader />
-          <Section style={styles.sectionWithBanner}>
-            <Text style={styles.text}>¡Hola {userName}!</Text>
-            <Text style={styles.text}>
-              Te recordamos que tu reserva para el espacio{" "}
-              <strong>
-                {task.reservation.stand.label}
-                {task.reservation.stand.standNumber}
-              </strong>{" "}
-              para nuestro próximo festival{" "}
-              <strong>{task.reservation.festival.name}</strong> aún está
-              pendiente de pago.
-            </Text>
-            <Text style={styles.text}>
-              Tu fecha límite de pago es el{" "}
-              <strong>
-                {formatDate(task.reservation.createdAt)
-                  .plus({ days: 5 })
-                  .toLocaleString(DateTime.DATE_MED)}
-              </strong>{" "}
-              a las{" "}
-              <strong>
-                {formatDate(task.reservation.createdAt)
-                  .plus({ days: 5 })
-                  .toLocaleString(DateTime.TIME_SIMPLE)}
-              </strong>
-              .
-            </Text>
-            <Text style={styles.text}>
-              Como se especifica en los términos y condiciones, toda reserva que
-              no sea confirmada dentro de las 120 horas desde su creación será
-              eliminada automáticamente.
-            </Text>
-            <Text style={styles.text}>
-              Si ya hiciste tu pago, subiste el comprobante a la página web y
-              estás esperando confirmación, podés ignorar este correo. Las
-              reservas pueden tomar hasta 48 horas en confirmarse pero el
-              registro del pago evitará que sea eliminada.
-            </Text>
-            <Text style={styles.text}>
-              Si tenés alguna duda, no dudes en contactarnos a nuesto correo
-              electrónico{" "}
-              <span style={styles.email}>soporte@productoraglitter.com</span>
-            </Text>
-          </Section>
-        </Container>
-        <EmailFooter />
-      </Body>
-    </Html>
-  );
+		<Html>
+			<Head />
+			<Preview>
+				Recuerda hacer el pago de tu reserva para{" "}
+				{task.reservation.festival.name}
+			</Preview>
+			<Body style={styles.main}>
+				<Container style={styles.container}>
+					<EmailHeader />
+					<Section style={styles.sectionWithBanner}>
+						<Text style={styles.text}>¡Hola {userName}!</Text>
+						<Text style={styles.text}>
+							Te recordamos que tu reserva para el espacio{" "}
+							<strong>
+								{task.reservation.stand.label}
+								{task.reservation.stand.standNumber}
+							</strong>{" "}
+							para nuestro próximo festival{" "}
+							<strong>{task.reservation.festival.name}</strong> aún está
+							pendiente de pago.
+						</Text>
+						<Text style={styles.text}>
+							Tu fecha límite de pago es el{" "}
+							<strong>
+								{formatDate(task.reservation.createdAt)
+									.plus({ days: 5 })
+									.toLocaleString(DateTime.DATE_MED)}
+							</strong>{" "}
+							a las{" "}
+							<strong>
+								{formatDate(task.reservation.createdAt)
+									.plus({ days: 5 })
+									.toLocaleString(DateTime.TIME_SIMPLE)}
+							</strong>
+							.
+						</Text>
+						<Text style={styles.text}>
+							Como se especifica en los términos y condiciones, toda reserva que
+							no sea confirmada dentro de las 120 horas desde su creación será
+							eliminada automáticamente.
+						</Text>
+						<Text style={styles.text}>
+							Si ya hiciste tu pago, subiste el comprobante a la página web y
+							estás esperando confirmación, podés ignorar este correo. Las
+							reservas pueden tomar hasta 48 horas en confirmarse pero el
+							registro del pago evitará que sea eliminada.
+						</Text>
+						<Text style={styles.text}>
+							Si tenés alguna duda, no dudes en contactarnos a nuesto correo
+							electrónico{" "}
+							<span style={styles.email}>soporte@productoraglitter.com</span>
+						</Text>
+					</Section>
+				</Container>
+				<EmailFooter />
+			</Body>
+		</Html>
+	);
 }
 
 ReservationReminderTemplate.PreviewProps = {
