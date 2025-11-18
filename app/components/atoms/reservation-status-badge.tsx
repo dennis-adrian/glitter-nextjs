dbea2fa33008d5483d86da502230451b08fe2602
dennis-adrian <dennisguzmanbo@gmail.com>
2025-11-18 16:19:09 -0400

    DEV-163: Add reservation status to participation history

--- app/components/atoms/reservation-status-badge.tsx
+++ app/components/atoms/reservation-status-badge.tsx
@@ -2,26 +2,32 @@ import { ReservationBase } from "@/app/api/reservations/definitions";
 import { Badge } from "@/app/components/ui/badge";
 
 const statusColors = {
-	pending: "bg-gray-500 hover:bg-gray-400",
-	accepted: "bg-green-500 hover:bg-green-400",
-	verification_payment: "bg-blue-500 hover:bg-blue-400",
-	rejected: "bg-red-600 hover:bg-red-500",
+	pending:
+		"bg-gray-500/20 border border-gray-300 text-gray-800 hover:bg-gray-500/30 hover:border-gray-300",
+	accepted:
+		"bg-green-500/20 border border-green-300 text-green-800 hover:bg-green-500/30 hover:border-green-300",
+	verification_payment:
+		"bg-blue-500/20 border border-blue-300 text-blue-800 hover:bg-blue-500/30 hover:border-blue-300",
+	rejected:
+		"bg-red-500/20 border border-red-300 text-red-800 hover:bg-red-500/30 hover:border-red-300",
 };
 
 const statusLabels = {
 	pending: "Pendiente",
 	accepted: "Confirmada",
 	verification_payment: "Verificación de Pago",
-	rejected: "Rechazada",
+	rejected: "Cancelada",
 };
 
 export default function ReservationStatusBadge({
 	status,
+	className,
 }: {
 	status: ReservationBase["status"];
+	className?: string;
 }) {
 	return (
-		<Badge className={`${statusColors[status]} font-normal`}>
+		<Badge className={`${statusColors[status]} font-normal ${className}`}>
 			{statusLabels[status]}
 		</Badge>
 	);
--- app/components/reservations/table.tsx
+++ app/components/reservations/table.tsx
@@ -1,88 +1,87 @@
 import { fetchReservations } from "@/app/api/reservations/actions";
+import ReservationStatusBadge from "@/app/components/atoms/reservation-status-badge";
 import ProfileCell from "@/app/components/common/table/profile-cell";
 import { ActionsCell } from "@/app/components/reservations/cells/actions";
 import PaymentStatus from "@/app/components/reservations/cells/payment-status";
-import { ReservationStatus } from "@/app/components/reservations/cells/status";
-import { Avatar, AvatarImage } from "@/app/components/ui/avatar";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from "@/app/components/ui/table";
 import { HeaderCell } from "@/app/components/users/header-cell";
 import { formatDate } from "@/app/lib/formatters";
 import { DateTime } from "luxon";
 
 type ReservationsTableProps = {
-  query?: string;
-  festivalId?: number;
+	query?: string;
+	festivalId?: number;
 };
 export default async function ReservationsTable(props: ReservationsTableProps) {
-  const reservations = await fetchReservations({
-    ...props,
-  });
+	const reservations = await fetchReservations({
+		...props,
+	});
 
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <HeaderCell value="number" label="#" />
-          <HeaderCell value="profile" label="Perfil" />
-          <HeaderCell value="status" label="Estado de la Reserva" />
-          <HeaderCell value="stand" label="Espacio" />
-          <HeaderCell value="festivalName" label="Festival" />
-          <HeaderCell value="paymentStatus" label="Estado de Pago" />
-          <HeaderCell value="createdAt" label="Fecha de Creación" />
-          <TableHead className="sticky right-0 z-20 bg-white shadow-inner"></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {reservations.length > 0 ? (
-          reservations.map((reservation, index) => (
-            <TableRow key={reservation.id}>
-              <TableCell>{index + 1}</TableCell>
-              <TableCell>
-                <div className="flex flex-col gap-2">
-                  {reservation.participants.map((participant) => (
-                    <ProfileCell
-                      key={participant.id}
-                      profile={participant.user}
-                    />
-                  ))}
-                </div>
-              </TableCell>
-              <TableCell>
-                <ReservationStatus reservation={reservation} />
-              </TableCell>
-              <TableCell>
-                {reservation.stand.label}
-                {reservation.stand.standNumber}
-              </TableCell>
-              <TableCell>{reservation.festival.name}</TableCell>
-              <TableCell>
-                <PaymentStatus reservation={reservation} />
-              </TableCell>
-              <TableCell>
-                {formatDate(reservation.createdAt).toLocaleString(
-                  DateTime.DATETIME_SHORT_WITH_SECONDS,
-                )}
-              </TableCell>
-              <TableCell>
-                <ActionsCell reservation={reservation} />
-              </TableCell>
-            </TableRow>
-          ))
-        ) : (
-          <TableRow>
-            <TableCell colSpan={6} className="h-24 text-center">
-              Sin resultados
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-  );
+	return (
+		<Table>
+			<TableHeader>
+				<TableRow>
+					<HeaderCell value="number" label="#" />
+					<HeaderCell value="profile" label="Perfil" />
+					<HeaderCell value="status" label="Estado de la Reserva" />
+					<HeaderCell value="stand" label="Espacio" />
+					<HeaderCell value="festivalName" label="Festival" />
+					<HeaderCell value="paymentStatus" label="Estado de Pago" />
+					<HeaderCell value="createdAt" label="Fecha de Creación" />
+					<TableHead className="sticky right-0 z-20 bg-white shadow-inner"></TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				{reservations.length > 0 ? (
+					reservations.map((reservation, index) => (
+						<TableRow key={reservation.id}>
+							<TableCell>{index + 1}</TableCell>
+							<TableCell>
+								<div className="flex flex-col gap-2">
+									{reservation.participants.map((participant) => (
+										<ProfileCell
+											key={participant.id}
+											profile={participant.user}
+										/>
+									))}
+								</div>
+							</TableCell>
+							<TableCell>
+								<ReservationStatusBadge status={reservation.status} />
+							</TableCell>
+							<TableCell>
+								{reservation.stand.label}
+								{reservation.stand.standNumber}
+							</TableCell>
+							<TableCell>{reservation.festival.name}</TableCell>
+							<TableCell>
+								<PaymentStatus reservation={reservation} />
+							</TableCell>
+							<TableCell>
+								{formatDate(reservation.createdAt).toLocaleString(
+									DateTime.DATETIME_SHORT_WITH_SECONDS,
+								)}
+							</TableCell>
+							<TableCell>
+								<ActionsCell reservation={reservation} />
+							</TableCell>
+						</TableRow>
+					))
+				) : (
+					<TableRow>
+						<TableCell colSpan={6} className="h-24 text-center">
+							Sin resultados
+						</TableCell>
+					</TableRow>
+				)}
+			</TableBody>
+		</Table>
+	);
 }
