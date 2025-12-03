dbefd089734a5c9967af4150fae1da31a3e3a07d
dennis-adrian <dennisguzmanbo@gmail.com>
2025-12-03 12:15:16 -0400

    DEV-168: Add page for best stand activity

---
+++ app/components/pages/festival_activities/best-stand-activity.tsx
@@ -0,0 +1,187 @@
+import { BaseProfile } from "@/app/api/users/definitions";
+import DateSpan from "@/app/components/atoms/date-span";
+import Title from "@/app/components/atoms/title";
+import EnrollRedirectButton from "@/app/components/festivals/festival_activities/enroll-redirect-button";
+import { FestivalActivityWithDetailsAndParticipants } from "@/app/lib/festivals/definitions";
+
+type BestStandActivityPageProps = {
+	activity: FestivalActivityWithDetailsAndParticipants;
+	currentProfile: BaseProfile;
+	forProfile: BaseProfile;
+};
+
+export default function BestStandActivityPage({
+	activity,
+	currentProfile,
+	forProfile,
+}: BestStandActivityPageProps) {
+	return (
+		<div className="container p-3 md:p-6 flex flex-col gap-4 md:gap-5">
+			<section>
+				<Title level="h1">{activity.name}</Title>
+				<p>{activity.description}</p>
+			</section>
+			<section>
+				<Title level="h3">¿En qué consiste la actividad?</Title>
+				<div className="flex flex-col gap-1 md:gap-2">
+					<p>
+						<em>Iconic Stand</em> es un reconocimiento especial para celebrar a
+						los expositores que llenan de creatividad, encanto y personalidad
+						sus espacios dentro del festival.
+					</p>
+					<p>
+						En esta edición del festival, los propios participantes votarán en
+						nuestro sitio web para elegir al stand que más brille por su
+						propuesta visual y originalidad.
+					</p>
+					<p>
+						El participante que reciba más votos en cada categoría obtendrá un
+						espacio sin costo en el primer Glitter 2026, como una forma de
+						celebrar su dedicación y el aporte que hace a la magia del festival.
+					</p>
+				</div>
+			</section>
+			<section>
+				<Title level="h3">¿Cómo participar?</Title>
+				<div className="flex flex-col gap-1 md:gap-2">
+					<p>
+						Si tenés una reserva confirmada para este festival, podrás
+						inscribirte para participar de la votación el{" "}
+						<strong>
+							6 de diciembre a partir de las 13:00 hasta las 15:00
+						</strong>
+						. Al inscribirte, deberás subir una imagen de tu stand para que los
+						demás participantes puedan verlo y votar por ti. La imagen tiene que
+						ser tomada el mismo día del festival. No puede ser una imagen
+						antigua ya que el propósito de la actividad es reconocer al stand
+						destacado de la edición actual.
+					</p>
+					<p>
+						No hay un límite de inscripciones. Todos los participantes pueden
+						inscribirse a la actividad. Habrá una votación por cada categoría
+						para elegir al <em>Iconic Stand</em> de cada una. Pero tomá en
+						cuenta que <strong>la participación es por stand</strong>. Si sos
+						ilustrador y compartís stand con otro ilustrador, solo uno de los
+						dos deberá inscribirse para participar de la actividad.
+					</p>
+					<p>El botón de inscripción se encuentra al final de la página.</p>
+				</div>
+			</section>
+			<section>
+				<Title level="h3">¿Cómo funciona la votación?</Title>
+				<div>
+					<p>
+						La votación se llevará a cabo a través de un sistema de votos
+						online. Existirán 3 categorías distintas por las cuales votar:{" "}
+						<strong>Ilustración</strong>, <strong>Gastronomía</strong> y{" "}
+						<strong>Emprendimiento creativo</strong>. Todos los participantes
+						deberán elegir su stand favorito en cada categoría. La votación
+						estará disponible solo para quienes tienen una reserva confirmada
+						para este festival a partir del{" "}
+						{activity.votingStartDate && activity.votingEndDate && (
+							<span>
+								<strong>
+									<DateSpan
+										date={activity.votingStartDate}
+										format={{ month: "long", day: "numeric" }}
+									/>
+								</strong>{" "}
+								a las{" "}
+								<strong>
+									<DateSpan
+										date={activity.votingStartDate}
+										format={{ hour: "numeric", minute: "numeric" }}
+									/>
+								</strong>{" "}
+								hasta el{" "}
+								<strong>
+									<DateSpan
+										date={activity.votingEndDate}
+										format={{ month: "long", day: "numeric" }}
+									/>
+								</strong>{" "}
+								a las{" "}
+								<strong>
+									<DateSpan
+										date={activity.votingEndDate}
+										format={{ hour: "numeric", minute: "numeric" }}
+									/>
+								</strong>
+								.
+							</span>
+						)}
+					</p>
+					<p>
+						A los participantes con más votos en cada categoría se los
+						reconocerá con un espacio sin costo en el primer festival Glitter
+						del 2026. En caso de ser ilustrador y compartir stand con otro
+						ilustrador, ambos deberán hacer su reserva en conjunto nuevamente
+						para recibir el espacio.
+					</p>
+					<p>
+						Los stands con más votos serán anunciados en el escenario y en la
+						comunidad de WhatsApp de la Productora Glitter antes de finalizar el
+						festival.
+					</p>
+				</div>
+			</section>
+			<section>
+				<Title level="h3">Resumen de condiciones para participar</Title>
+				<ol className="ml-2 list-decimal list-inside space-y-2">
+					<li>Tener una reserva confirmada para este festival.</li>
+					<li>
+						Inscribirse a la actividad con el botón de inscripción que se
+						encuentra al final de la página.
+					</li>
+					<li>
+						Subir una imagen de tu stand{" "}
+						{activity.proofUploadLimitDate && (
+							<span>
+								hasta el{" "}
+								<strong>
+									<DateSpan
+										date={activity.proofUploadLimitDate}
+										format={{ month: "long", day: "numeric" }}
+									/>
+								</strong>{" "}
+								a las{" "}
+								<strong>
+									<DateSpan
+										date={activity.proofUploadLimitDate}
+										format={{ hour: "numeric", minute: "numeric" }}
+									/>
+								</strong>
+							</span>
+						)}{" "}
+						para que los demás participantes puedan verlo y votar por ti.
+					</li>
+					<li>
+						Votar por tu stand favorito en cada categoría durante el periodo de
+						votación. No se permitirá votar por uno mismo.
+					</li>
+					<li>
+						Cumplir todas las normas del evento según los términos y condiciones
+						aceptados al inscribirse al festival.
+					</li>
+				</ol>
+				<p className="text-sm text-muted-foreground italic flex flex-col gap-1 mt-2">
+					<span>
+						* Si un participante incumple las normas del evento, podrá ser
+						eliminado de la actividad.
+					</span>
+					<span>
+						* En caso de recibir el reconocimiento del <em>Iconic Stand</em>,
+						dependiendo de la gravedad de la norma infringida, el reconocimiento
+						podría ser retirado.
+					</span>
+				</p>
+			</section>
+			<EnrollRedirectButton
+				currentProfile={currentProfile}
+				forProfile={forProfile}
+				festivalId={activity.festivalId}
+				activity={activity}
+			/>
+		</div>
+	);
+}
