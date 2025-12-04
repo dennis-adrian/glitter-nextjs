3e7d85dcba67abeb58afdf7fb644db776a1c3069
dennis-adrian <dennisguzmanbo@gmail.com>
2025-12-04 10:56:47 -0400

    DEV-169: Add minimal voting page

---
+++ app/(routes)/profiles/[profileId]/festivals/[festivalId]/activity/[activityId]/voting/page.tsx
@@ -0,0 +1,36 @@
+import Title from "@/app/components/atoms/title";
+import { fetchFestivalActivity } from "@/app/lib/festival_activites/actions";
+import { getCurrentUserProfile, protectRoute } from "@/app/lib/users/helpers";
+import { notFound } from "next/navigation";
+import { z } from "zod";
+
+const ParamsSchema = z.object({
+	profileId: z.coerce.number(),
+	festivalId: z.coerce.number(),
+	activityId: z.coerce.number(),
+});
+
+type VotingPageProps = {
+	params: Promise<z.infer<typeof ParamsSchema>>;
+};
+
+export default async function VotingPage({ params }: VotingPageProps) {
+	const validatedParams = ParamsSchema.safeParse(await params);
+	if (!validatedParams.success) return notFound();
+
+	const { profileId, festivalId, activityId } = validatedParams.data;
+
+	const currentProfile = await getCurrentUserProfile();
+	if (!currentProfile) return notFound();
+
+	await protectRoute(currentProfile || undefined, profileId);
+
+	const activity = await fetchFestivalActivity(activityId);
+	if (!activity || !activity.allowsVoting) return notFound();
+
+	return (
+		<div className="container p-3 md:p-6">
+			<Title level="h1">Votación para {activity.name}</Title>
+		</div>
+	);
+}
