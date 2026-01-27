467dbe3488f6795b3d9e8677fa4d5a71c2b900ab
dennis-adrian <dennisguzmanbo@gmail.com>
2026-01-26 23:04:43 -0400

    GDEV-4: Rename a couple of config files

---
+++ next.config.ts
@@ -0,0 +1,63 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "img.clerk.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "files.edgestore.dev",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "utfs.io",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "ja4q35y666.ufs.sh",
+//       },
+//     ],
+//   },
+//   logging: {
+//     fetches: {
+//       fullUrl: true,
+//     },
+//   },
+// };
+
+// module.exports = nextConfig;
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	// reactCompiler: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "img.clerk.com",
+			},
+			{
+				protocol: "https",
+				hostname: "files.edgestore.dev",
+			},
+			{
+				protocol: "https",
+				hostname: "utfs.io",
+			},
+			{
+				protocol: "https",
+				hostname: "ja4q35y666.ufs.sh",
+			},
+		],
+	},
+	logging: {
+		fetches: {
+			fullUrl: true,
+		},
+	},
+};
+
+export default nextConfig;
